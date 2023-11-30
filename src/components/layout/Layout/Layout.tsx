f8e5a53de0e8fc5febd64a6991f25af8de33ccb3
Brian Cooper <brian@brian-cooper.com>
2023-11-30 03:47:09 -0600

    feature(layout): add typed props

--- src/components/layout/Layout/Layout.tsx
+++ src/components/layout/Layout/Layout.tsx
@@ -4,16 +4,21 @@ import { Flex, Grid, chakra } from "@chakra-ui/react";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 
-import Footer from "../Footer/Footer";
-import Header from "../Header/Header";
+import { Footer, Header } from "components/layout";
 import app from "lib/config/app.config";
 
+import type { ReactNode } from "react";
+
 dayjs.extend(relativeTime);
+
+interface Props {
+  children: ReactNode;
+}
 
 /**
  * Core application layout.
  */
-const Layout = ({ children }: any) => (
+const Layout = ({ children }: Props) => (
   <Grid templateRows="auto 1fr auto" w="100%" h="100%">
     <Flex
       p={4}
