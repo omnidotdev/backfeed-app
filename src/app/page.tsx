3adee0e9e26478bb44db680236eaa630e06efcfc
Brian Cooper <brian@brian-cooper.com>
2024-02-16 05:54:41 -0600

    chore: update home page copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { app } from "lib/config";
 
 const HomePage = () => {
-  return <div>{app.name} is ...</div>;
+  return <div>{app.name} is a feedback portal for the decentralized web.</div>;
 };
 
 export default HomePage;
