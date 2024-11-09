51395ce646448f5a4ad36cd358721aeb0d83a3d4
Brian Cooper <brian@brian-cooper.com>
2024-11-09 13:15:09 -0600

    chore: remove Alchemy API key env var

--- src/lib/config/env.config.ts
+++ src/lib/config/env.config.ts
@@ -3,5 +3,4 @@ export const NODE_ENV = process.env.NODE_ENV;
 export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 // web3
-export const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 export const WALLETCONNECT_PROJECT_ID = process.env.WALLETCONNECT_PROJECT_ID;
