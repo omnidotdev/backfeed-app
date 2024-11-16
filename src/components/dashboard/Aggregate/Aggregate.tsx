740a8c9932488d059aa32befef48e2cc25729e0e
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-11-16 14:09:14 -0600

    refactor(aggregate): adjust skeletons to only wrap dynamic data

--- src/components/dashboard/Aggregate/Aggregate.tsx
+++ src/components/dashboard/Aggregate/Aggregate.tsx
@@ -10,35 +10,37 @@ interface Props {
 }
 
 const Aggregate = ({ title, value, icon, isLoaded = true }: Props) => (
-  <Skeleton isLoaded={isLoaded}>
-    <Flex
-      direction="column"
-      gap={3}
-      borderColor="border.subtle"
-      p={6}
-      bgColor="background.default"
-      borderRadius="lg"
-      boxShadow="lg"
-    >
-      <Flex align="center" justify="space-between" gap={2}>
-        <Text color="foreground.subtle" fontSize="sm" fontWeight="medium">
-          {title}
-        </Text>
+  <Flex
+    direction="column"
+    gap={3}
+    borderColor="border.subtle"
+    p={6}
+    bgColor="background.default"
+    borderRadius="lg"
+    boxShadow="lg"
+  >
+    <Flex align="center" justify="space-between" gap={2}>
+      <Text color="foreground.subtle" fontSize="sm" fontWeight="medium">
+        {title}
+      </Text>
 
-        <Icon src={icon} w={5} h={5} color="foreground.subtle" />
-      </Flex>
+      <Icon src={icon} w={5} h={5} color="foreground.subtle" />
+    </Flex>
 
-      <Flex direction="column" gap={1}>
+    <Flex direction="column" gap={1}>
+      <Skeleton isLoaded={isLoaded} maxW={!isLoaded ? 32 : undefined}>
         <Text fontSize="2xl" fontWeight="semibold" lineHeight={1.2}>
           {value}
         </Text>
+      </Skeleton>
 
+      <Skeleton isLoaded={isLoaded} maxW={!isLoaded ? 40 : undefined}>
         <Text fontSize="xs" color="green" fontWeight="medium">
           +4.2069% from last month
         </Text>
-      </Flex>
+      </Skeleton>
     </Flex>
-  </Skeleton>
+  </Flex>
 );
 
 export default Aggregate;
