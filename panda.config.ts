a90c5fa47899942326b0c0902217249032a2f40a
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-11-17 15:44:06 -0600

    chore(panda): add comment about additional staticCss value

--- panda.config.ts
+++ panda.config.ts
@@ -13,6 +13,7 @@ const pandaConfig = defineConfig({
         properties: {
           color: ["*"],
           backgroundColor: ["*"],
+          // NB: added to render dynamic border colors in `Response` component. Without this, the border color is static (pulled from recipe) unless i.e. `borderColor="blue"` is explciitly used elsewhere in the app.
           borderColor: ["*"],
         },
       },
