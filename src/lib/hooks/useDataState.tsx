f300aaceae3916a28ecb98e85540996fd8006203
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-11-17 17:03:37 -0600

    docs(hooks): add JSDoc for useDataState
    Co-authored-by: Brian Cooper <20056195+coopbri@users.noreply.github.com>

--- src/lib/hooks/useDataState.tsx
+++ src/lib/hooks/useDataState.tsx
@@ -2,14 +2,20 @@ import { useTimeout } from "@omnidev/sigil";
 import { useState } from "react";
 
 interface DataState {
+  /** Whether the data is loading. */
   isLoading: boolean;
+  /** Whether an error was encountered while loading the data. */
   isError?: boolean;
 }
 
 interface Options {
+  /** Timeout to simulate asynchronous data fetching. Defaults to 300 (ms). */
   timeout?: number;
 }
 
+/**
+ * Access data lifecycle state.
+ */
 const useDataState = ({ timeout = 300 }: Options = {}) => {
   const [dataState, setDataState] = useState<DataState>({
     isLoading: true,
