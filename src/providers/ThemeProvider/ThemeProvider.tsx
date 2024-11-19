a81427bf2c22cfada000a8ce478a00e29d5a9cc2
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-11-19 11:11:26 -0600

    docs: add JSDoc for ThemeProvider

--- src/providers/ThemeProvider/ThemeProvider.tsx
+++ src/providers/ThemeProvider/ThemeProvider.tsx
@@ -2,6 +2,9 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 import type { PropsWithChildren } from "react";
 
+/**
+ * Application theme provider.
+ */
 const ThemeProvider = ({ children }: PropsWithChildren) => (
   <NextThemesProvider defaultTheme="system" attribute="class">
     {children}
