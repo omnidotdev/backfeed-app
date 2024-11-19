bff8ce876f20f0c5fddcd25f67999d22d56f496e
Beau Hawkinson <72956780+Twonarly1@users.noreply.github.com>
2024-11-19 09:33:35 -0600

    chore: add Organization tpye with jsdoc

--- src/components/dashboard/Organizations/Organizations.tsx
+++ src/components/dashboard/Organizations/Organizations.tsx
@@ -14,51 +14,60 @@ import { OrganizationCard } from "components/dashboard";
 import { app } from "lib/config";
 import { useDataState } from "lib/hooks";
 
-const allOrganizations = [
+interface Organization {
+  /** Organization ID. */
+  id: string;
+  /** Organization name. */
+  name: string;
+  /** Organization type. */
+  type: string;
+}
+
+const allOrganizations: Organization[] = [
   {
+    id: "8af2410c-b73b-453f-a5c9-4637f5cbaffe",
     name: "Tech Innovators Inc.",
     type: "Technology",
-    id: "8af2410c-b73b-453f-a5c9-4637f5cbaffe",
   },
   {
+    id: "c630fc16-1bb7-474f-9405-89401cce301a",
     name: "Green Future Solutions",
     type: "Environmental Services",
-    id: "c630fc16-1bb7-474f-9405-89401cce301a",
   },
   {
+    id: "aff499bc-516f-436c-9d87-2edfe1043061",
     name: "EduSpark Academy",
     type: "Education",
-    id: "aff499bc-516f-436c-9d87-2edfe1043061",
   },
   {
+    id: "f13625fe-c2f9-47b7-a2b0-4e2073289122",
     name: "HealthPlus Clinics",
     type: "Healthcare",
-    id: "f13625fe-c2f9-47b7-a2b0-4e2073289122",
   },
   {
+    id: "99411095-e652-444a-a37c-5579dd15f463",
     name: "Urban Architects Co.",
     type: "Architecture",
-    id: "99411095-e652-444a-a37c-5579dd15f463",
   },
   {
+    id: "4ee643e8-4290-4182-bd69-513f623a7415",
     name: "Creative Minds Studio",
     type: "Design",
-    id: "4ee643e8-4290-4182-bd69-513f623a7415",
   },
   {
+    id: "076ccbba-de01-45db-b0fb-586553baaa8f",
     name: "MarketTrail Consulting",
     type: "Consulting",
-    id: "076ccbba-de01-45db-b0fb-586553baaa8f",
   },
   {
+    id: "1f7014fe-b766-4879-ae3e-f0e619397585",
     name: "Global Reach Logistics",
     type: "Logistics",
-    id: "1f7014fe-b766-4879-ae3e-f0e619397585",
   },
   {
+    id: "06157ff2-0959-4edc-87c5-5a6db8c551de",
     name: "Peak Performance Sports",
     type: "Sports Management",
-    id: "06157ff2-0959-4edc-87c5-5a6db8c551de",
   },
 ];
 
@@ -118,7 +127,7 @@ const Organizations = () => {
       </Flex>
 
       <Grid gap={6} alignItems="center" columns={{ base: 1, md: 3 }}>
-        {pinnedOrganizations.map(({ name, type, id }) => (
+        {pinnedOrganizations.map(({ id, name, type }) => (
           <OrganizationCard
             key={id}
             name={name}
