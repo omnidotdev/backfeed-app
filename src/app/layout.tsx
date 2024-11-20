e13d5c9510a49e290487d83d3a47ad76a5dc41df
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-11-19 19:58:03 -0600

    Fix theme flashing on refresh (#33)
    * refactor(providers): use next-themes to handle theme toggle
    
    * refactor(theme-provider): default theme to users system preference
    
    * chore: add comment about suppressHydrationWarning
    
    * docs: add JSDoc for ThemeProvider
    
    * fix: remove unused export
    
    * refactor(theme-provider): disable transitions on change
    
    * refactor(knip): add appropriate project file definition

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,8 @@ export const metadata: Metadata = {
 };
 
 const RootLayout = ({ children }: { children: ReactNode }) => (
-  <html lang="en">
+  // !!NB: suppressHydrationWarning is required for next-themes to work properly. This property only applies one level deep, so it won't block hydration warnings on other elements. See https://github.com/pacocoursey/next-themes?tab=readme-ov-file#use for more details
+  <html lang="en" suppressHydrationWarning>
     <head>
       <link
         rel="icon"
