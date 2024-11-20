ae7513aa6f3beee2d28eb065500d7d36f19ac12f
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-11-20 08:28:38 -0600

    refactor(not-found): use VStack pattern for less prop management

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,24 +1,24 @@
 "use client";
 
-import { Button, Stack, Text } from "@omnidev/sigil";
+import { Button, Text, VStack } from "@omnidev/sigil";
 import Link from "next/link";
 
 /**
  * Global 404 (not found error) page.
  */
 const GlobalNotFound = () => (
-  <Stack align="center" justify="center" gap={4} h="full">
-    <Stack align="center">
+  <VStack justify="center" gap={4} h="full">
+    <VStack>
       <Text fontSize={80} color="foreground.disabled">
         404
       </Text>
       <Text>Page Not Found</Text>
-    </Stack>
+    </VStack>
 
     <Link href="/">
       <Button>Return home</Button>
     </Link>
-  </Stack>
+  </VStack>
 );
 
 export default GlobalNotFound;
