3ef6b271fd5715b38f018c3ac59a81afbebcdcd7
Brian Cooper <brian@brian-cooper.com>
2024-12-23 15:03:07 -0600

    chore: remove get started CTA component

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -1,7 +1,6 @@
 export { default as AccountInformation } from "./AccountInformation/AccountInformation";
 export { default as ErrorBoundary } from "./ErrorBoundary/ErrorBoundary";
 export { default as Footer } from "./Footer/Footer";
-export { default as GetStarted } from "./GetStarted/GetStarted";
 export { default as Header } from "./Header/Header";
 export { default as Layout } from "./Layout/Layout";
 export { default as Page } from "./Page/Page";
