cdb9da954845c7d3256725b75b786f86819cb13a
Brian Cooper <brian@brian-cooper.com>
2024-12-26 10:48:31 -0600

    style(font): remove Rubik

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import Providers from "app/providers";
 import { Layout } from "components/layout";
 import { app } from "lib/config";
-import fonts from "lib/theme/fonts";
 
 import type { Metadata } from "next";
 import type { ReactNode } from "react";
@@ -35,7 +34,7 @@ const RootLayout = ({ children }: { children: ReactNode }) => (
       <link rel="shortcut icon" href="/favicon.ico" />
     </head>
 
-    <body className={fonts.primary.className}>
+    <body>
       <Providers>
         <Layout>{children}</Layout>
       </Providers>
