9c9bdf0b81ac4f6a1c8dd566137064eec0fdcf28
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-12-28 19:13:33 -0600

    refactor(recent-feedback): update props for response

--- src/components/dashboard/RecentFeedback/RecentFeedback.tsx
+++ src/components/dashboard/RecentFeedback/RecentFeedback.tsx
@@ -5,77 +5,10 @@ import { Flex } from "@omnidev/sigil";
 import { SkeletonArray } from "components/core";
 import { FeedbackCard, Response } from "components/dashboard";
 import { ErrorBoundary } from "components/layout";
+import { useRecentFeedbackQuery } from "generated/graphql";
 import { useAuth } from "lib/hooks";
 
-import type { ResponseType } from "components/dashboard";
-import { useRecentFeedbackQuery } from "generated/graphql";
-
-interface Feedback {
-  id: string;
-  sender: string;
-  message: string;
-  date: string;
-  type: ResponseType;
-}
-
-const FEEDBACK: Feedback[] = [
-  {
-    id: "1",
-    sender: "Back Feed",
-    message: "I still like turtles.",
-    date: "30 seconds ago",
-    type: "Neutral",
-  },
-  {
-    id: "2",
-    sender: "Feed Back",
-    message: "The new dashboard layout is much more intuitive!",
-    date: "4 minutes ago",
-    type: "Positive",
-  },
-  {
-    id: "3",
-    sender: "Fed Front",
-    message: "Having issues with the new export feature.",
-    date: "3 hours ago",
-    type: "Bug",
-  },
-  {
-    id: "4",
-    sender: "Back Fed",
-    message: "Would love to be able to export feedback.",
-    date: "2 days ago",
-    type: "Feature",
-  },
-  {
-    id: "5",
-    sender: "Back Feed",
-    message: "I am having troubles logging in.",
-    date: "3 days ago",
-    type: "Bug",
-  },
-  {
-    id: "6",
-    sender: "Back Fed",
-    message: "I love turtles!",
-    date: "4 days ago",
-    type: "Positive",
-  },
-  {
-    id: "7",
-    sender: "Front Fed",
-    message: "Would love to be able to export feedback.",
-    date: "10 days ago",
-    type: "Feature",
-  },
-  {
-    id: "8",
-    sender: "Back Feed",
-    message: "I like turtles.",
-    date: "69 days ago",
-    type: "Neutral",
-  },
-];
+import type { Post } from "generated/graphql";
 
 /**
  * Recent feedback section.
@@ -117,9 +50,7 @@ const RecentFeedback = () => {
             recentFeedback?.map((feedback) => (
               <Response
                 key={feedback?.rowId}
-                sender={feedback?.user?.rowId}
-                message={feedback?.description}
-                date={feedback?.createdAt}
+                feedback={feedback as Partial<Post>}
                 // TODO: make this dynamic once the data is streamed in
                 type="Neutral"
               />
--- src/components/dashboard/Response/Response.tsx
+++ src/components/dashboard/Response/Response.tsx
@@ -4,25 +4,23 @@ import { Badge, Flex, Text } from "@omnidev/sigil";
 import dayjs from "dayjs";
 import { match } from "ts-pattern";
 
+import type { Post } from "generated/graphql";
+
 // NB: tried to use an enum here but had difficulties with runtime errors
 export type ResponseType = "Neutral" | "Positive" | "Bug" | "Feature";
 
 // NB: this prop drilling is under the assumption that the query from parent won't provide much overhead (i.e. parent is isolated query and has minimal nesting / a response is a direct child)
 interface Props {
-  /** Feedback sender. */
-  sender: string | undefined;
-  /** Feedback message. */
-  message: string | null | undefined;
-  /** Date feedback was published. */
-  date: Date | null | undefined;
-  /** Feedback type (i.e. category). */
-  type: ResponseType | undefined;
+  /** Feedback details. */
+  feedback: Partial<Post>;
+  /** Feedback type */
+  type: ResponseType;
 }
 
 /**
  * Recent feedback response.
  */
-const Response = ({ sender, message, date, type }: Props) => {
+const Response = ({ feedback, type }: Props) => {
   const color = match(type)
     .with("Neutral", () => "foreground.subtle")
     .with("Positive", () => "green")
@@ -41,7 +39,7 @@ const Response = ({ sender, message, date, type }: Props) => {
       <Flex direction="column">
         <Flex align="center" justify="space-between">
           <Text fontWeight="semibold" fontSize="sm" mb={1}>
-            {sender}
+            {feedback?.rowId}
           </Text>
 
           <Badge color={color} borderColor={color}>
@@ -50,12 +48,12 @@ const Response = ({ sender, message, date, type }: Props) => {
         </Flex>
 
         <Text fontSize="sm" color="foreground.subtle">
-          {message}
+          {feedback?.description}
         </Text>
       </Flex>
 
       <Text fontSize="xs" color="foreground.muted">
-        {dayjs(date).fromNow()}
+        {dayjs(feedback?.createdAt).fromNow()}
       </Text>
     </Flex>
   );
