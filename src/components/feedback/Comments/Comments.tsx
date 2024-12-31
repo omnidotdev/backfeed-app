8aaee0546399a650ff3003e1439bf991f0c6d3a9
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-12-31 10:58:02 -0600

    refactor(comments): integrate useInfiniteCommentsQuery

--- src/components/feedback/Comments/Comments.tsx
+++ src/components/feedback/Comments/Comments.tsx
@@ -9,19 +9,14 @@ import {
   Textarea,
   VStack,
 } from "@omnidev/sigil";
-import { useInfiniteQuery } from "@tanstack/react-query";
-import request from "graphql-request";
-import { useCallback } from "react";
 import { LuMessageSquare } from "react-icons/lu";
 import useInfiniteScroll from "react-infinite-scroll-hook";
 
 import { SkeletonArray, Spinner } from "components/core";
 import { CommentCard } from "components/feedback";
 import { EmptyState, ErrorBoundary, SectionContainer } from "components/layout";
-import { CommentsDocument } from "generated/graphql";
-import { API_BASE_URL, app } from "lib/config";
-
-import type { CommentsQuery, CommentsQueryVariables } from "generated/graphql";
+import { useInfiniteCommentsQuery } from "generated/graphql";
+import { app } from "lib/config";
 
 interface Props {
   /** Feedback ID. */
@@ -32,46 +27,31 @@ interface Props {
  * Feedback comments section.
  */
 const Comments = ({ feedbackId }: Props) => {
-  const variables: CommentsQueryVariables = {
-    pageSize: 5,
-    feedbackId,
-  };
-
-  // TODO: Discuss using `useInfiniteCommentsQuery`. Any insight on how to appropriately apply the dynamic `after` parameter to that provided hook would be much appreciated.
   const { data, isLoading, isError, hasNextPage, fetchNextPage } =
-    useInfiniteQuery<CommentsQuery>({
-      queryKey: ["comments", variables],
-      queryFn: ({ pageParam }) =>
-        request({
-          url: API_BASE_URL!,
-          document: CommentsDocument,
-          variables: {
-            ...variables,
-            after: pageParam,
-          },
-        }),
-      initialPageParam: undefined,
-      getNextPageParam: (lastPage) =>
-        lastPage?.comments?.pageInfo?.hasNextPage
-          ? lastPage?.comments?.pageInfo?.endCursor
-          : undefined,
-    });
+    useInfiniteCommentsQuery(
+      {
+        pageSize: 5,
+        feedbackId,
+      },
+      {
+        initialPageParam: undefined,
+        getNextPageParam: (lastPage) =>
+          lastPage?.comments?.pageInfo?.hasNextPage
+            ? { after: lastPage?.comments?.pageInfo?.endCursor }
+            : undefined,
+      }
+    );
 
+  // These are not defined within the `select` function in order to preserve type safety.
   const totalCount = data?.pages?.[0]?.comments?.totalCount ?? 0;
   const comments = data?.pages?.flatMap((page) =>
     page?.comments?.edges?.map((edge) => edge?.node)
   );
 
-  const loadMoreComments = useCallback(() => {
-    if (!hasNextPage) return;
-
-    fetchNextPage();
-  }, [hasNextPage, fetchNextPage]);
-
   const [loaderRef, { rootRef }] = useInfiniteScroll({
     loading: isLoading,
     hasNextPage: hasNextPage,
-    onLoadMore: loadMoreComments,
+    onLoadMore: fetchNextPage,
     disabled: isError,
     // NB: `rootMargin` is passed to `IntersectionObserver`. We can use it to trigger 'onLoadMore' when the spinner comes *near* to being visible, instead of when it becomes fully visible within the root element.
     rootMargin: "0px 0px 400px 0px",
