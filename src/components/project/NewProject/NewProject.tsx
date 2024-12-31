37c43e6431e542ebbe991b475f8eedc4f42b1b3f
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2024-12-31 12:01:51 -0600

    refactor(new-project): add select component for organization selection

--- src/components/project/NewProject/NewProject.tsx
+++ src/components/project/NewProject/NewProject.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import { Dialog } from "@omnidev/sigil";
+import { Dialog, Select, createListCollection } from "@omnidev/sigil";
+import { useOrganizationsQuery } from "generated/graphql";
 
 import { app } from "lib/config";
+import { useAuth } from "lib/hooks";
 
 interface Props {
   /** State to determine if the dialog is open. */
@@ -14,13 +16,43 @@ interface Props {
 /**
  * Dialog for creating a new project.
  */
-const NewProject = ({ isOpen, setIsOpen }: Props) => (
-  <Dialog
-    title={app.dashboardPage.cta.newProject.label}
-    description={app.dashboardPage.cta.newProject.description}
-    open={isOpen}
-    onOpenChange={({ open }) => setIsOpen(open)}
-  />
-);
+const NewProject = ({ isOpen, setIsOpen }: Props) => {
+  const { user } = useAuth();
+  
+  const { data: organizations } = useOrganizationsQuery({
+    userId: user?.id!,
+  }, {
+    enabled: !!user,
+    select: (data) => data?.organizations?.nodes?.map((organization) => ({
+      label: organization?.name,
+      value: organization?.rowId,
+    })),
+  })
+  
+  return (
+    <Dialog
+      title={app.dashboardPage.cta.newProject.label}
+      description={app.dashboardPage.cta.newProject.description}
+      open={isOpen}
+      onOpenChange={({ open }) => setIsOpen(open)}
+    >
+      <Select
+        label={{
+          id: "organizations",
+          singular: "Organization",
+          plural: "Organizations",
+        }}
+        collection={createListCollection({ items: organizations ?? []})}
+        displayGroupLabel={false}
+        valueTextProps={{
+          placeholder: "Select an organization",
+        }}
+        triggerProps={{
+          borderColor: "border.subtle",
+        }}
+      />
+    </Dialog>
+  )
+};
 
 export default NewProject;
