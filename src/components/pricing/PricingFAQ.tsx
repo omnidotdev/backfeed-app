cf5983f743cdae2a85d0e9ffa59287742b49e6a8
benjamin-parks <benparks93@gmail.com>
2024-12-31 16:27:41 -0600

    chore: format

--- src/components/pricing/PricingFAQ.tsx
+++ src/components/pricing/PricingFAQ.tsx
@@ -5,28 +5,28 @@ import { app } from "lib/config";
  * Frequently asked questions about pricing.
  */
 const PricingFAQ = () => (
-    <Stack
-      w="45%"
-      m="auto"
-      flexWrap="nowrap"
-      flexDirection="column"
-      justifyContent="center"
-      alignItems="center"
-      mt={2}
+  <Stack
+    w="45%"
+    m="auto"
+    flexWrap="nowrap"
+    flexDirection="column"
+    justifyContent="center"
+    alignItems="center"
+    mt={2}
+  >
+    <Text
+      as="h2"
+      fontSize="2xl"
+      fontWeight="bold"
+      textAlign="center"
+      mb={4}
+      mt={8}
     >
-      <Text
-        as="h2"
-        fontSize="2xl"
-        fontWeight="bold"
-        textAlign="center"
-        mb={4}
-        mt={8}
-      >
-        {app.pricingPage.pricingFAQ.FAQ}
-      </Text>
-      
-      <Accordion items={app.pricingPage.pricingFAQ.items} />
-    </Stack>
+      {app.pricingPage.pricingFAQ.FAQ}
+    </Text>
+
+    <Accordion items={app.pricingPage.pricingFAQ.items} />
+  </Stack>
 );
 
 export default PricingFAQ;
