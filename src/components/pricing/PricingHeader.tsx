ee872dac5efc33b71b3a46552cd545d6644d6b9e
benjamin-parks <benparks93@gmail.com>
2024-12-31 15:10:43 -0600

    chore: format

--- src/components/pricing/PricingHeader.tsx
+++ src/components/pricing/PricingHeader.tsx
@@ -2,20 +2,20 @@ import { Flex, Text } from "@omnidev/sigil";
 import { app } from "lib/config";
 
 const PricingHeader = () => (
-    <Flex align="center" direction="column" w="90%">
-      <Text
-        as="h1"
-        fontSize="5xl"
-        fontWeight="bold"
-        textAlign="center"
-        marginTop="4"
-      >
-        {app.pricingPage.pricingHeader.title}
-      </Text>
-      <Text fontSize="2xl" textAlign={"center"} marginBottom={"8"}>
-        {app.pricingPage.pricingHeader.description}
-      </Text>
-    </Flex>
-  );
+  <Flex align="center" direction="column" w="90%">
+    <Text
+      as="h1"
+      fontSize="5xl"
+      fontWeight="bold"
+      textAlign="center"
+      marginTop="4"
+    >
+      {app.pricingPage.pricingHeader.title}
+    </Text>
+    <Text fontSize="2xl" textAlign={"center"} marginBottom={"8"}>
+      {app.pricingPage.pricingHeader.description}
+    </Text>
+  </Flex>
+);
 
 export default PricingHeader;
