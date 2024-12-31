08f3adc9138592f1466270cd00bfb1397c7e6cdd
Ben Parks <65567300+benjamin-parks@users.noreply.github.com>
2024-12-31 14:53:10 -0600

    Update src/components/pricing/index.tsx
    Co-authored-by: Brian Cooper <20056195+coopbri@users.noreply.github.com>

--- src/components/pricing/index.tsx
+++ src/components/pricing/index.tsx
@@ -1,3 +1,3 @@
-export { default as PricingHeader } from "./PricingHeader";
 export { default as PricingCard } from "./PricingCard";
 export { default as PricingFAQ } from "./PricingFAQ";
+export { default as PricingHeader } from "./PricingHeader";
