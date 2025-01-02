37ee1b1c7a701c354e2bebb0158d88e9d85f92f7
Brian Cooper <brian@brian-cooper.com>
2025-01-02 15:14:14 -0600

    test(e2e,auth): add basic auth (sign in) test

---
+++ src/__tests__/auth.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from "@playwright/test";
+
+if (!process.env.TEST_USERNAME || !process.env.TEST_PASSWORD) {
+  throw new Error("TEST_USERNAME and TEST_PASSWORD must be set");
+}
+
+test("authenticates user (sign in)", async ({ page }) => {
+  await page.goto("/");
+
+  page.locator("text=Sign In").click();
+
+  await page.waitForURL("https://hidra.omni.dev/**");
+
+  await page.locator("#username").fill(process.env.TEST_USERNAME!);
+  await page.locator("#password").fill(process.env.TEST_PASSWORD!);
+  await page.getByRole("button", { name: "Sign In" }).click();
+
+  expect(page.url()).toBe("http://localhost:3000/");
+});
