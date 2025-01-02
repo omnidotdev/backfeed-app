58cba31a26bb84785ff07d2de83b0ee7311de610
Beau Hawkinson <72956780+Twonarly1@users.noreply.github.com>
2025-01-02 12:38:35 -0600

    chore: remove unused export helper functions

--- src/lib/util/index.ts
+++ src/lib/util/index.ts
@@ -1,7 +1,5 @@
 export { default as getAuthSession } from "./getAuthSession/getAuthSession";
-export { default as getOrganization } from "./getOrganization/getOrganization";
 export {
   default as getResponseTypeColor,
   type ResponseType,
 } from "./getPostTypeColor/getPostTypeColor";
-export { default as getProject } from "./getProject/getProject";
