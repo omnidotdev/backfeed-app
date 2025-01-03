12c2d9550cc56e5489964392bee8a5b0d984f202
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-01-03 12:42:26 -0600

    refactor: adjust pattern for FormFieldError

--- src/components/core/FormFieldError/FormFieldError.tsx
+++ src/components/core/FormFieldError/FormFieldError.tsx
@@ -2,19 +2,23 @@
 
 import { Text } from "@omnidev/sigil";
 
+import type { TagVariants, TextProps } from "@omnidev/sigil";
 import type { ValidationError } from "@tanstack/react-form";
 
-interface Props {
+// NB: Omitting `as` and reapplying it to the `Text` component is to avoid ts errors as `TextProps` is not statically typed.
+interface Props extends Omit<TextProps, "as"> {
   /** Error message. */
   error: ValidationError | undefined;
   /** Dirty state of the form field. */
   isDirty: boolean;
+  /** What HTML element to render the error text as. */
+  as?: TagVariants;
 }
 
 /**
  * Text to be displayed when a form field has errors.
  */
-const FormFieldError = ({ error, isDirty }: Props) => (
+const FormFieldError = ({ error, isDirty, as = "p", ...rest }: Props) => (
   <Text
     position="absolute"
     top={-0.5}
@@ -23,6 +27,8 @@ const FormFieldError = ({ error, isDirty }: Props) => (
     fontSize="sm"
     color="red"
     visibility={error && isDirty ? "visible" : "hidden"}
+    as={as}
+    {...rest}
   >
     {error}
   </Text>
