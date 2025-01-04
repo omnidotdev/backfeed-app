8c219574add3783431b7839be432d86785283443
Brian Cooper <brian@brian-cooper.com>
2025-01-03 21:20:12 -0600

    fix: fix glob expansion for non-zsh shells

--- src/scripts/execute_tests.sh
+++ src/scripts/execute_tests.sh
@@ -2,4 +2,4 @@
 # Execute tests with Bun.
 # NB: Bun test execution is handled by this script because of issues with patterns (glob expansion) not working properly across shells when test patterns are passed directly in `package.json` scripts. This file can be removed in favor of a direct `package.json` script once resolved. See https://discord.com/channels/876711213126520882/1184686223185432688/1184686223185432688
 
-bun test $(find src -type f \( -name "*.test.ts" -o -name "*.test.tsx" \))
+find src -type f \( -name "*.test.ts" -o -name "*.test.tsx" \) | xargs bun test
