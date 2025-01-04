615ea462525a3dc849528b88871953da86fbb2a3
Brian Cooper <brian@brian-cooper.com>
2025-01-03 20:37:31 -0600

    refactor(env): update exports, knip ignores

--- src/lib/config/env.config.ts
+++ src/lib/config/env.config.ts
@@ -1,7 +1,6 @@
 // core
-/** @knipignore TODO remove this directive once `NODE_ENV` is used */
-export const NODE_ENV = process.env.NODE_ENV;
-export const APP_ENV = process.env.APP_ENV;
+const NODE_ENV = process.env.NODE_ENV;
+const APP_ENV = process.env.APP_ENV;
 export const NEXT_RUNTIME = process.env.NEXT_RUNTIME;
 export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
@@ -12,7 +11,9 @@ export const WALLETCONNECT_PROJECT_ID = process.env.WALLETCONNECT_PROJECT_ID;
 export const MOCK_USER_ID = process.env.NEXT_PUBLIC_MOCK_USER_ID;
 
 // simplified environment helpers
+/** @knipignore TODO remove this directive once `isProdEnv` is used */
 export const isProdEnv = NODE_ENV === "production";
+/** @knipignore TODO remove this directive once `isDevEnv` is used */
 export const isDevEnv = NODE_ENV === "development";
 // NB: `APP_ENV` is used instead of `NODE_ENV` because `next dev` shadows `NODE_ENV`, so even if `NODE_ENV=test` is injected into the environment, it will be overwritten to "development". See https://github.com/vercel/next.js/issues/17032
 export const isTestEnv = APP_ENV === "test";
