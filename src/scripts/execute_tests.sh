c9aec7a5bed36d0d6c992a84084eae53ed213274
Brian Cooper <brian@brian-cooper.com>
2025-01-03 21:18:20 -0600

    fix: fix glob expansion for non-zsh shells

--- src/scripts/execute_tests.sh
+++ src/scripts/execute_tests.sh
@@ -2,5 +2,4 @@
 # Execute tests with Bun.
 # NB: Bun test execution is handled by this script because of issues with patterns (glob expansion) not working properly across shells when test patterns are passed directly in `package.json` scripts. This file can be removed in favor of a direct `package.json` script once resolved. See https://discord.com/channels/876711213126520882/1184686223185432688/1184686223185432688
 
-# NB: `globstar` must be explicitly enabled for proper glob expansion
-shopt -s globstar && bun test src/**/*.test.{ts,tsx} $1
+bun test $(find src -type f \( -name "*.test.ts" -o -name "*.test.tsx" \))
