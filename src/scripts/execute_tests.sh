fa9c9be4635bf7e38192b3bdf3fcb30d0ed5b8d1
Brian Cooper <brian@brian-cooper.com>
2025-01-03 20:58:22 -0600

    refactor(scripts,test): 'zsh' -> 'sh'

--- src/scripts/execute_tests.sh
+++ src/scripts/execute_tests.sh
@@ -1,5 +1,5 @@
-#!/bin/zsh
+#!/bin/sh
 # Execute tests with Bun.
 # NB: Bun test execution is handled by this script because of issues with patterns (glob expansion) not working properly across shells when test patterns are passed directly in `package.json` scripts. This file can be removed in favor of a direct `package.json` script once resolved. See https://discord.com/channels/876711213126520882/1184686223185432688/1184686223185432688
 
-bun test src/**/*.test.(ts|tsx) $1
+bun test src/**/*.test.{ts,tsx} $1
