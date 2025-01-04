0b0a32deeac2cdc31bdf121a18113f55a273d483
Brian Cooper <brian@brian-cooper.com>
2025-01-03 21:02:08 -0600

    test: use zsh

--- src/scripts/execute_tests.sh
+++ src/scripts/execute_tests.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/zsh
 # Execute tests with Bun.
 # NB: Bun test execution is handled by this script because of issues with patterns (glob expansion) not working properly across shells when test patterns are passed directly in `package.json` scripts. This file can be removed in favor of a direct `package.json` script once resolved. See https://discord.com/channels/876711213126520882/1184686223185432688/1184686223185432688
 
