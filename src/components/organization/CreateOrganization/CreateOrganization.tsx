46a95c0e8663c07257886d3a50c3e74720abb67a
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-01-07 21:36:06 -0600

    chore: update comment regarding onChangeAsync validator
    Co-authored-by: Brian Cooper <20056195+coopbri@users.noreply.github.com>

--- src/components/organization/CreateOrganization/CreateOrganization.tsx
+++ src/components/organization/CreateOrganization/CreateOrganization.tsx
@@ -181,7 +181,7 @@ const CreateOrganization = ({ isOpen, setIsOpen }: Props) => {
         <Field
           name="slug"
           asyncDebounceMs={300}
-          // onChange validation is used here to keep in sync with the async form level validation of the slug field
+          // `onChangeAsync` validation is used here to keep in sync with the async form level validation of the slug field
           validators={{
             onChangeAsync: baseSchema.shape.slug,
           }}
