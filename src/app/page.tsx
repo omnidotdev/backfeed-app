9bbee5e43c592ed53ea64d61b38812e3de249528
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-01-09 17:38:07 -0600

    feature(dashboard): prefetch pinned organizations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,42 @@
+import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+
 import { DashboardPage } from "components/dashboard";
 import { LandingPage } from "components/landing";
+import { OrganizationOrderBy, useOrganizationsQuery } from "generated/graphql";
 import { getAuthSession } from "lib/util";
+
+import type { OrganizationsQueryVariables } from "generated/graphql";
 
 /**
  * Home page. This route is dynamically rendered based on the user's authentication status.
  */
 const HomePage = async () => {
+  const queryClient = new QueryClient();
+  
   const session = await getAuthSession();
+
+  
+  if (session) {
+    const variables: OrganizationsQueryVariables = {
+      pageSize: 3,
+      offset: 0,
+      orderBy: [OrganizationOrderBy.UserOrganizationsCountDesc],
+      userId: session.user.rowId!,
+    };
+    
+    await queryClient.prefetchQuery({
+      queryKey: useOrganizationsQuery.getKey(variables),
+      queryFn: useOrganizationsQuery.fetcher(variables)
+    })
+  }
 
   if (!session) return <LandingPage />;
 
-  return <DashboardPage />;
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <DashboardPage />
+    </HydrationBoundary>
+  );
 };
 
 export default HomePage;
