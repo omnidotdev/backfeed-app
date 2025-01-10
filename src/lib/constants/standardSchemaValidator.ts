845a29abe20900ad292d0cb96dbedb81d21a835c
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-01-09 21:43:09 -0600

    refactor: adjust casing for schema validator import

--- src/lib/constants/standardSchemaValidator.ts
+++ src/lib/constants/standardSchemaValidator.ts
@@ -1,9 +1,9 @@
-import { standardSchemaValidator as ReactFormStandardSchemaValidator } from "@tanstack/react-form";
+import { standardSchemaValidator as reactFormStandardSchemaValidator } from "@tanstack/react-form";
 
 /**
  * Custom validator adapter. Set up so that only the first error message is returned. This is to ensure that the error message(s) are displayed to the user in a consistent way.
  */
-const standardSchemaValidator = ReactFormStandardSchemaValidator({
+const standardSchemaValidator = reactFormStandardSchemaValidator({
   transformErrors: (issues) => issues.map((issue) => issue.message)[0],
 });
 
