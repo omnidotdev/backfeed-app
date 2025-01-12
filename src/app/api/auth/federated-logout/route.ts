be29d97a0226e8beffd63fcd83124fea1eb12c60
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-01-12 14:35:19 -0600

    docs: update JSDoc for federated logout route handler

--- src/app/api/auth/federated-logout/route.ts
+++ src/app/api/auth/federated-logout/route.ts
@@ -2,7 +2,7 @@ import { auth } from "auth";
 import { AUTH_KEYCLOAK_ISSUER, BASE_URL } from "lib/config";
 
 /**
- * Federated logout route handler. This route is used to handle the backchannel logout flow.
+ * Federated logout route handler. This route is used to construct the redirect URL for the backchannel logout flow.
  */
 export const GET = async () => {
   let redirectURL = BASE_URL!;
