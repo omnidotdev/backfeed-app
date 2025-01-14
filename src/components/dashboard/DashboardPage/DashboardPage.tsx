d99dc868df3b84e429c247ec4022a983092f67d4
Beau Hawkinson <72956780+Twonarly1@users.noreply.github.com>
2025-01-14 13:34:14 -0600

    Update src/components/dashboard/DashboardPage/DashboardPage.tsx

--- src/components/dashboard/DashboardPage/DashboardPage.tsx
+++ src/components/dashboard/DashboardPage/DashboardPage.tsx
@@ -74,7 +74,6 @@ const DashboardPage = () => {
             // TODO: get Sigil Icon component working and update accordingly. Context: https://github.com/omnidotdev/backfeed-app/pull/44#discussion_r1897974331
             icon: <LuPlusCircle />,
             dialogType: DialogType.CreateOrganization,
-            disabled: !numberOfOrganizations,
           },
           {
             label: app.dashboardPage.cta.newProject.label,
