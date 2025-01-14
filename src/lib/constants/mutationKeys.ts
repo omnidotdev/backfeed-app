336e8b5c53e32857404297634cafbdeabe95abd5
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-01-13 23:29:31 -0600

    chore: format

--- src/lib/constants/mutationKeys.ts
+++ src/lib/constants/mutationKeys.ts
@@ -1,4 +1,7 @@
 import { useCreateProjectMutation } from "generated/graphql";
 
 const PROJECT_MUTATION_KEY = ["project"];
-export const CREATE_PROJECT_MUTATION_KEY = [...PROJECT_MUTATION_KEY, ...useCreateProjectMutation.getKey()];
+export const CREATE_PROJECT_MUTATION_KEY = [
+  ...PROJECT_MUTATION_KEY,
+  ...useCreateProjectMutation.getKey(),
+];
