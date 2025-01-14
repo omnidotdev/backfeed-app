b1dc0a24780e9b4dbcc110e45f8c8d2d4da66c0b
Beau Hawkinson <72956780+Twonarly1@users.noreply.github.com>
2025-01-14 12:23:30 -0600

    Update src/components/core/CallToAction/CallToAction.tsx
    Co-authored-by: hobbescodes <87732294+hobbescodes@users.noreply.github.com>

--- src/components/core/CallToAction/CallToAction.tsx
+++ src/components/core/CallToAction/CallToAction.tsx
@@ -42,8 +42,8 @@ const CallToAction = ({ action, ...rest }: Props) => {
       return;
     }
 
-    if (dialogType && dialogStore?.setIsOpen) {
-      dialogStore.setIsOpen(true);
+    if (dialogType) {
+      setIsOpen(true);
     }
   };
 
