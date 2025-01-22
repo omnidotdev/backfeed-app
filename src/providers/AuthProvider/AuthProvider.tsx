3665579db42fc8881cc4002ec61d9d8e20a1acdb
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-01-21 18:12:24 -0600

    refactor(auth-provider): update refetch parameters

--- src/providers/AuthProvider/AuthProvider.tsx
+++ src/providers/AuthProvider/AuthProvider.tsx
@@ -2,11 +2,20 @@ import { SessionProvider as AuthSessionProvider } from "next-auth/react";
 
 import type { PropsWithChildren } from "react";
 
+// 5 minutes
+// NB: this should match the expiry time of the access token received from the IDP provider.
+const DEFAULT_REFETCH_INTERVAL = 5 * 60;
+
 /**
  * Auth session provider.
  */
 const AuthProvider = ({ children }: PropsWithChildren) => (
-  <AuthSessionProvider>{children}</AuthSessionProvider>
+  <AuthSessionProvider
+    refetchInterval={DEFAULT_REFETCH_INTERVAL}
+    refetchOnWindowFocus={false}
+  >
+    {children}
+  </AuthSessionProvider>
 );
 
 export default AuthProvider;
