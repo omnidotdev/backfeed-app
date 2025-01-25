8f0a3c48d80edf9462f661df8ca3e88655e26596
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-01-25 10:53:07 -0600

    refactor: remove unused export on isTestEnv

--- src/lib/config/env.config.ts
+++ src/lib/config/env.config.ts
@@ -8,8 +8,7 @@ export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 export const isProdEnv = NODE_ENV === "production";
 export const isDevEnv = NODE_ENV === "development";
 // NB: `APP_ENV` is used instead of `NODE_ENV` because `next dev` shadows `NODE_ENV`, so even if `NODE_ENV=test` is injected into the environment, it will be overwritten to "development". See https://github.com/vercel/next.js/issues/17032
-/** @knipignore `isTestEnv` is used below, but Knip doesn't detect it */
-export const isTestEnv = APP_ENV === "test";
+const isTestEnv = APP_ENV === "test";
 
 // tests
 // enable mock service worker (https://mswjs.io/docs/integrations/browser#conditionally-enable-mocking), this is wrapped in case mocking requests and responses during development is desired
