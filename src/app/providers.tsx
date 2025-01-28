413b495e0698d7b0bb216dd9c04230467f4265e5
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-01-28 09:53:52 -0600

    chore: add context to AuthProvider comment

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -18,7 +18,7 @@ interface Props extends PropsWithChildren {
  * Application context providers.
  */
 const Providers = ({ session, children }: Props) => (
-  // NB: key is used to force a re-mount when session changes
+  // NB: key is used to force a re-mount when session changes, See: https://github.com/nextauthjs/next-auth/issues/9504#issuecomment-2326123445
   <AuthProvider key={session?.user?.id} session={session}>
     <ThemeProvider>
       <SearchParamsProvider>
