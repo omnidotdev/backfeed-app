97f9ec012315e0672d0d9795040c70fc919460fa
Brian Cooper <brian@brian-cooper.com>
2025-01-29 16:52:41 -0600

    feature(pricing): add sample matrix values

--- src/components/pricing/PricingMatrix/PricingMatrix.tsx
+++ src/components/pricing/PricingMatrix/PricingMatrix.tsx
@@ -1,25 +1,50 @@
 import {
+  Icon,
   Table,
   TableCell,
   TableHeader,
   type TableProps,
   TableRow,
 } from "@omnidev/sigil";
+import { FaCheck, FaX } from "react-icons/fa6";
+
+const COMMON_FEATURES = {
+  gdpr: { label: "GDPR Compliance", value: true },
+  communitySupport: { label: "Community Support", value: true },
+};
 
 const productData = [
   {
     id: "basic",
     name: "Basic",
+    features: {
+      ...COMMON_FEATURES,
+      // webhooks: { label: "Webhooks", value: false },
+    },
   },
   {
     id: "professional",
     name: "Professional",
+    features: {
+      ...COMMON_FEATURES,
+      webhooks: { label: "Webhooks", value: true },
+      apiAccess: { label: "API Access", value: true },
+    },
   },
   {
     id: "enterprise",
     name: "Enterprise",
+    features: {
+      ...COMMON_FEATURES,
+      webhooks: { label: "Webhooks", value: true },
+      apiAccess: { label: "API Access", value: true },
+    },
   },
 ] as const;
+
+const allFeatures = Array.from(
+  new Set(productData.flatMap(({ features }) => Object.keys(features))),
+);
 
 const headerProps = {
   fontWeight: "bold",
@@ -48,9 +73,24 @@ const PricingMatrix = (props: TableProps) => {
       }
       {...props}
     >
-      {productData.map(({ id, name }) => (
-        <TableRow key={id}>
-          <TableCell fontWeight="medium">wip</TableCell>
+      {allFeatures.map((feature) => (
+        <TableRow key={feature}>
+          <TableCell textAlign="center" fontWeight="semibold" fontSize="xl">
+            {/* @ts-ignore TODO */}
+            {productData.find((p) => p.features[feature])?.features[feature]
+              ?.label || feature}
+          </TableCell>
+
+          {productData.map(({ id, features }) => (
+            <TableCell key={id} textAlign="center">
+              {/* @ts-ignore TODO */}
+              {features[feature]?.value ? (
+                <Icon src={FaCheck} color="green.500" />
+              ) : (
+                <Icon src={FaX} color="red.500" />
+              )}
+            </TableCell>
+          ))}
         </TableRow>
       ))}
     </Table>
