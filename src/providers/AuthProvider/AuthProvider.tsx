0dd4962966c26ce67384fa85797b61a311677951
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-02-06 19:56:52 -0600

    refactor: memoize session key

--- src/providers/AuthProvider/AuthProvider.tsx
+++ src/providers/AuthProvider/AuthProvider.tsx
@@ -1,19 +1,20 @@
 import { SessionProvider as AuthSessionProvider } from "next-auth/react";
+import { useMemo } from "react";
 
 import type { SessionProviderProps } from "next-auth/react";
 
 /**
  * Auth session provider.
  */
-const AuthProvider = ({ session, children, ...rest }: SessionProviderProps) => (
-  // NB: key is used to force a re-mount when session changes, See: https://github.com/nextauthjs/next-auth/issues/9504#issuecomment-2326123445
-  <AuthSessionProvider
-    key={session?.user?.customerId}
-    session={session}
-    {...rest}
-  >
-    {children}
-  </AuthSessionProvider>
-);
+const AuthProvider = ({ session, children, ...rest }: SessionProviderProps) => {
+  const sessionKey = useMemo(() => session?.user?.customerId, [session]);
+
+  return (
+    // NB: key is used to force a re-mount when customerId changes, See: https://github.com/nextauthjs/next-auth/issues/9504#issuecomment-2326123445
+    <AuthSessionProvider key={sessionKey} session={session} {...rest}>
+      {children}
+    </AuthSessionProvider>
+  );
+};
 
 export default AuthProvider;
