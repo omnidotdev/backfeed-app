845cdc4c0548c7052e8ccf76ac3d317473330923
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-02-12 17:19:46 -0600

    docs(hooks): add JSDoc to returns for useOrganizationMembership

--- src/lib/hooks/useOrganizationMembership.tsx
+++ src/lib/hooks/useOrganizationMembership.tsx
@@ -21,9 +21,21 @@ const useOrganizationMembership = ({ userId, organizationId }: Options) => {
   );
 
   return {
+    /**
+     * Organization membership ID for the user. If the user is not a member, this will be undefined.
+     */
     membershipId: data?.rowId,
+    /**
+     * Boolean indicating if the user has ownership permissions for the organization (owner role only).
+     */
     isOwner: data?.role === Role.Owner,
+    /**
+     * Boolean indicating if the user has admin permissions for the organization (admin or owner role).
+     */
     isAdmin: data?.role === Role.Admin || data?.role === Role.Owner,
+    /**
+     * Boolean indicating if the user is a member of the organization.
+     */
     isMember: data != null,
   };
 };
