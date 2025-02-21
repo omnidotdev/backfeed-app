7fe2165e778fb937e9f1ffb90085cee55a56d521
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-02-21 12:54:27 -0600

    Update pattern for handling search parameters (#84)
    * fix: debounce callback for inputs that update search params
    
    * fix: apply useHandleSearch to project search
    
    * refactor(panda): add overscroll settings
    
    * refactor(project): remove disabled props from create feedback elements
    
    * refactor(next): remove unnecessary webpack configuration from config

--- next.config.ts
+++ next.config.ts
@@ -8,16 +8,6 @@ const nextConfig: NextConfig = {
     AUTH_KEYCLOAK_ID: process.env.AUTH_KEYCLOAK_ID,
     AUTH_KEYCLOAK_ISSUER: process.env.AUTH_KEYCLOAK_ISSUER,
   },
-  // https://github.com/vercel/next.js/issues/44273#issuecomment-1375170722
-  webpack: (config) => {
-    config.externals.push({
-      bufferutil: "commonjs bufferutil",
-      encoding: "commonjs encoding",
-      "supports-color": "commonjs supports-color",
-      "utf-8-validate": "commonjs utf-8-validate",
-    });
-    return config;
-  },
 };
 
 export default nextConfig;
