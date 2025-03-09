0392b9abbc186dfbe4128ee1afbbcfc917729c35
Brian Cooper <brian@brian-cooper.com>
2025-03-08 19:16:05 -0600

    refactor(pricing-matrix): convert to inline JSX return

--- src/components/pricing/PricingMatrix/PricingMatrix.tsx
+++ src/components/pricing/PricingMatrix/PricingMatrix.tsx
@@ -52,7 +52,7 @@ const tiers = [
 ] as const;
 
 const allFeatures = Array.from(
-  new Set(tiers.flatMap(({ features }) => Object.keys(features)))
+  new Set(tiers.flatMap(({ features }) => Object.keys(features))),
 );
 
 const headerProps = {
@@ -64,59 +64,57 @@ const headerProps = {
 /**
  * Pricing feature matrix.
  */
-const PricingMatrix = (props: TableProps) => {
-  return (
-    <Table
-      headerContent={
-        <TableRow>
-          <TableHeader {...headerProps}>Feature</TableHeader>
+const PricingMatrix = (props: TableProps) => (
+  <Table
+    headerContent={
+      <TableRow>
+        <TableHeader {...headerProps}>Feature</TableHeader>
 
-          {tiers
-            .filter(({ name }) => name)
-            .map(({ name }) => (
-              <TableHeader key={name} {...headerProps}>
-                {name}
-              </TableHeader>
-            ))}
-        </TableRow>
-      }
-      {...props}
-    >
-      {allFeatures.map((feature) => (
-        <TableRow key={feature}>
-          <TableCell
-            textAlign="center"
-            fontWeight="semibold"
-            fontSize="xl"
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-            gap={2}
-          >
-            {/* @ts-ignore TODO */}
-            {tiers.find((p) => p.features[feature])?.features[feature]?.label ||
-              feature}
-
-            {/* TODO condense with above */}
-            {/* @ts-ignore TODO */}
-            {tiers.find((p) => p.features[feature])?.features[feature]
-              ?.comingSoon && <Badge>Coming Soon</Badge>}
-          </TableCell>
-
-          {tiers.map(({ id, features }) => (
-            <TableCell key={id} textAlign="center">
-              {/* @ts-ignore TODO */}
-              {features[feature]?.value ? (
-                <Icon src={FaCheck} color="green.500" />
-              ) : (
-                <Icon src={FaX} color="red.500" />
-              )}
-            </TableCell>
+        {tiers
+          .filter(({ name }) => name)
+          .map(({ name }) => (
+            <TableHeader key={name} {...headerProps}>
+              {name}
+            </TableHeader>
           ))}
-        </TableRow>
-      ))}
-    </Table>
-  );
-};
+      </TableRow>
+    }
+    {...props}
+  >
+    {allFeatures.map((feature) => (
+      <TableRow key={feature}>
+        <TableCell
+          textAlign="center"
+          fontWeight="semibold"
+          fontSize="xl"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          gap={2}
+        >
+          {/* @ts-ignore TODO */}
+          {tiers.find((p) => p.features[feature])?.features[feature]?.label ||
+            feature}
+
+          {/* TODO condense with above */}
+          {/* @ts-ignore TODO */}
+          {tiers.find((p) => p.features[feature])?.features[feature]
+            ?.comingSoon && <Badge>Coming Soon</Badge>}
+        </TableCell>
+
+        {tiers.map(({ id, features }) => (
+          <TableCell key={id} textAlign="center">
+            {/* @ts-ignore TODO */}
+            {features[feature]?.value ? (
+              <Icon src={FaCheck} color="green.500" />
+            ) : (
+              <Icon src={FaX} color="red.500" />
+            )}
+          </TableCell>
+        ))}
+      </TableRow>
+    ))}
+  </Table>
+);
 
 export default PricingMatrix;
