5f8b3358f4602b87d6b0af04f13e3b1d79f2d00a
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-03-11 20:12:15 -0500

    docs(customer-portal): add JSDoc

--- src/components/profile/CustomerPortal/CustomerPortal.tsx
+++ src/components/profile/CustomerPortal/CustomerPortal.tsx
@@ -7,9 +7,13 @@ import { Link } from "components/core";
 import type { CustomerState } from "@polar-sh/sdk/models/components/customerstate";
 
 interface Props {
+  /** Customer information. */
   customer: CustomerState;
 }
 
+/**
+ * CustomerPortal component. Redirects to Polar's customer portal to manage subscriptions, purchases, etc.
+ */
 const CustomerPortal = ({ customer }: Props) => (
   <Link href={`/api/customer/portal?customerId=${customer.id}`}>
     <Button>Manage Subscriptions</Button>
