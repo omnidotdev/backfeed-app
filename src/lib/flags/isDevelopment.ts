4ae11284b6196da3b4837a33c386ffb0ba440bac
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-03-18 12:59:58 -0500

    docs(flags): add JSDoc to development flag

--- src/lib/flags/isDevelopment.ts
+++ src/lib/flags/isDevelopment.ts
@@ -2,6 +2,9 @@ import { flag } from "flags/next";
 
 import { isDevEnv } from "lib/config";
 
+/**
+ * Feature flag that determines if the application is running in development mode.
+ */
 const isDevelopment = flag({
   key: "development-flag",
   decide: () => isDevEnv,
