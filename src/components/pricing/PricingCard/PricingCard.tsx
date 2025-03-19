fdd6c8563c8a7e73a70aa995335d5a53d1c645c2
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-03-19 11:09:50 -0500

    refactor(pricing-card): use custom hook to derive metadata information

--- src/components/pricing/PricingCard/PricingCard.tsx
+++ src/components/pricing/PricingCard/PricingCard.tsx
@@ -16,12 +16,11 @@ import {
 import { SubscriptionRecurringInterval } from "@polar-sh/sdk/models/components/subscriptionrecurringinterval";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import { HiLockOpen, HiSparkles } from "react-icons/hi2";
 import { LuCheck, LuClockAlert } from "react-icons/lu";
-import { P, match } from "ts-pattern";
+import { match } from "ts-pattern";
 
 import { app } from "lib/config";
-import { useAuth, useSearchParams } from "lib/hooks";
+import { useAuth, useProductMetadata, useSearchParams } from "lib/hooks";
 
 import type { CardProps } from "@omnidev/sigil";
 import type { BenefitCustomProperties } from "@polar-sh/sdk/models/components/benefitcustomproperties";
@@ -33,11 +32,11 @@ const COMING_SOON = "coming soon";
 /**
  * Get a human-readable price.
  * @param price Fixed price details. Derived from product.
- * @param isEnterprise Whether the product is enterprise tier.
+ * @param isEnterpriseTier Whether the product is enterprise tier.
  * @returns A human-readable price.
  */
-const getPrice = (price: ProductPrice, isEnterprise: boolean) => {
-  if (price.amountType !== "fixed" || isEnterprise)
+const getPrice = (price: ProductPrice, isEnterpriseTier: boolean) => {
+  if (price.amountType !== "fixed" || isEnterpriseTier)
     return app.pricingPage.pricingCard.customPricing;
 
   return price.priceAmount / 100;
@@ -61,15 +60,8 @@ const PricingCard = ({ product, ...rest }: Props) => {
   const isPerMonthPricing =
     pricingModel === SubscriptionRecurringInterval.Month;
 
-  // ! NB: these metadata properties are typically optional and must be present for the corresponding product
-  const isRecommendedTier = !!product.metadata.isRecommended;
-  const isDisabled = !!product.metadata.isDisabled;
-  const isEnterprise = !!product.metadata.isEnterprise;
-
-  const actionIcon = match(product.metadata)
-    .with({ isRecommended: P.nonNullable }, () => HiSparkles)
-    .with({ isEnterprise: P.nonNullable }, () => undefined)
-    .otherwise(() => HiLockOpen);
+  const { isRecommendedTier, isEnterpriseTier, isDisabled, actionIcon } =
+    useProductMetadata({ product });
 
   return (
     <Card
@@ -130,11 +122,11 @@ const PricingCard = ({ product, ...rest }: Props) => {
 
           <HStack display="inline-flex" alignItems="center">
             <Text as="h3" fontSize="4xl" fontWeight="bold">
-              {!isEnterprise && <sigil.sup fontSize="lg">$</sigil.sup>}
-              {getPrice(product.prices[0] as ProductPrice, isEnterprise)}
+              {!isEnterpriseTier && <sigil.sup fontSize="lg">$</sigil.sup>}
+              {getPrice(product.prices[0] as ProductPrice, isEnterpriseTier)}
             </Text>
 
-            {!isEnterprise && (
+            {!isEnterpriseTier && (
               <sigil.span
                 fontSize="lg"
                 mt={2}
@@ -164,7 +156,7 @@ const PricingCard = ({ product, ...rest }: Props) => {
           >
             {actionIcon && <Icon src={actionIcon} h={4} w={4} />}
 
-            {isEnterprise
+            {isEnterpriseTier
               ? app.pricingPage.pricingCard.enterprise
               : app.pricingPage.pricingCard.getStarted}
           </Button>
---
+++ src/lib/hooks/useProductMetadata.tsx
@@ -0,0 +1,42 @@
+import { HiLockOpen, HiSparkles } from "react-icons/hi2";
+import { P, match } from "ts-pattern";
+
+import type { Product } from "@polar-sh/sdk/models/components/product";
+
+interface Options {
+  /** The product to fetch metadata for. */
+  product: Product;
+}
+
+/**
+ * Custom hook to define properties based on product metadata.
+ */
+const useProductMetadata = ({ product }: Options) => {
+  const metadata = product.metadata;
+
+  const isRecommendedTier = match(metadata)
+    .with({ isRecommended: P.nonNullable }, () => true)
+    .otherwise(() => false);
+
+  const isEnterpriseTier = match(metadata)
+    .with({ isEnterprise: P.nonNullable }, () => true)
+    .otherwise(() => false);
+
+  const isDisabled = match(metadata)
+    .with({ isDisabled: P.nonNullable }, () => true)
+    .otherwise(() => false);
+
+  const actionIcon = match(metadata)
+    .with({ isRecommended: P.nonNullable }, () => HiSparkles)
+    .with({ isEnterprise: P.nonNullable }, () => undefined)
+    .otherwise(() => HiLockOpen);
+
+  return {
+    isRecommendedTier,
+    isEnterpriseTier,
+    isDisabled,
+    actionIcon,
+  };
+};
+
+export default useProductMetadata;
