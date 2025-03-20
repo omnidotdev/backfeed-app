1303a15e761d6854c6bf71ea71e8db8359abd68f
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-03-20 11:14:01 -0500

    refactor(project-settings): handle switch toggles for default status

--- src/components/project/UpdateStatuses/UpdateStatuses.tsx
+++ src/components/project/UpdateStatuses/UpdateStatuses.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { parseColor } from "@ark-ui/react";
 import {
   Box,
@@ -10,11 +12,12 @@ import {
   Stack,
   Switch,
 } from "@omnidev/sigil";
+import { useState } from "react";
 import { HiOutlineEyeDropper } from "react-icons/hi2";
 
+import { SectionContainer } from "components/layout";
 import { useProjectStatusesQuery } from "generated/graphql";
 
-import { SectionContainer } from "components/layout";
 import type { Project } from "generated/graphql";
 
 const COLOR_PRESETS = [
@@ -55,6 +58,10 @@ const UpdateStatuses = ({ projectId }: Props) => {
     }
   );
 
+  const [defaultStatusId, setDefaultStatusId] = useState(
+    statuses?.find((status) => status?.isDefault)?.rowId
+  );
+
   return (
     <SectionContainer
       title="Project Statuses"
@@ -75,84 +82,91 @@ const UpdateStatuses = ({ projectId }: Props) => {
 
             return bValue - aValue;
           })
-          .map((status) => (
-            <Stack key={status.rowId} bgColor="background.default" p={4}>
-              <Stack gap={0.5}>
-                <HStack justify="space-between">
-                  <Label htmlFor={`status-${status.rowId}`}>Status</Label>
+          .map((status) => {
+            const isDefaultStatus = status.rowId === defaultStatusId;
 
-                  <Switch
-                    defaultChecked={status.isDefault ?? false}
-                    label={status.isDefault ? "Default" : undefined}
-                    size="sm"
-                    flexDirection="row-reverse"
-                    labelProps={{
-                      fontSize: "xs",
-                    }}
-                  />
-                </HStack>
+            return (
+              <Stack key={status.rowId} bgColor="background.default" p={4}>
+                <Stack gap={0.5}>
+                  <HStack justify="space-between">
+                    <Label htmlFor={`status-${status.rowId}`}>Status</Label>
 
-                <Input
-                  id={`status-${status.rowId}`}
-                  defaultValue={status.status}
-                  borderColor="border.subtle"
-                  size="sm"
-                />
-              </Stack>
-
-              <Stack gap={0.5}>
-                <Label htmlFor={`description-${status.rowId}`}>
-                  Description
-                </Label>
-                <Input
-                  id={`description-${status.rowId}`}
-                  defaultValue={status.description ?? ""}
-                  placeholder="Set a description for the status"
-                  borderColor="border.subtle"
-                  size="sm"
-                />
-              </Stack>
-
-              <ColorPicker
-                label="Color"
-                presets={COLOR_PRESETS}
-                defaultValue={
-                  status.color ? parseColor(status.color) : undefined
-                }
-                gap={0.5}
-                channelInputProps={{
-                  // TODO: Omit upstream, or make it optional
-                  channel: "hex",
-                  borderColor: "border.subtle",
-                  // @ts-ignore TODO: fix type error upstream. This `size` prop should be derived from `Input` due to the `asChild` prop. Works at runtime.
-                  size: "sm",
-                }}
-                triggerProps={{
-                  borderColor: "transparent",
-                  p: 0,
-                  // @ts-ignore TODO: fix type error upstream. This `size` prop should be derived from `Button` due to the `asChild` prop. Works at runtime.
-                  size: "sm",
-                }}
-                // @ts-ignore TODO: omit `value` upstream. The value is derived internally.
-                swatchProps={{
-                  h: "full",
-                  w: "full",
-                  borderRadius: "sm",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  children: (
-                    <Icon
-                      src={HiOutlineEyeDropper}
-                      color="background.default"
-                      h={5}
-                      w={5}
+                    <Switch
+                      checked={isDefaultStatus}
+                      onCheckedChange={({ checked }) =>
+                        checked ? setDefaultStatusId(status.rowId) : undefined
+                      }
+                      label={isDefaultStatus ? "Default" : undefined}
+                      size="sm"
+                      flexDirection="row-reverse"
+                      labelProps={{
+                        fontSize: "xs",
+                      }}
                     />
-                  ),
-                }}
-              />
-            </Stack>
-          ))}
+                  </HStack>
+
+                  <Input
+                    id={`status-${status.rowId}`}
+                    defaultValue={status.status}
+                    borderColor="border.subtle"
+                    size="sm"
+                  />
+                </Stack>
+
+                <Stack gap={0.5}>
+                  <Label htmlFor={`description-${status.rowId}`}>
+                    Description
+                  </Label>
+                  <Input
+                    id={`description-${status.rowId}`}
+                    defaultValue={status.description ?? ""}
+                    placeholder="Set a description for the status"
+                    borderColor="border.subtle"
+                    size="sm"
+                  />
+                </Stack>
+
+                <ColorPicker
+                  label="Color"
+                  presets={COLOR_PRESETS}
+                  defaultValue={
+                    status.color ? parseColor(status.color) : undefined
+                  }
+                  gap={0.5}
+                  channelInputProps={{
+                    // TODO: Omit upstream, or make it optional
+                    channel: "hex",
+                    borderColor: "border.subtle",
+                    // @ts-ignore TODO: fix type error upstream. This `size` prop should be derived from `Input` due to the `asChild` prop. Works at runtime.
+                    size: "sm",
+                  }}
+                  triggerProps={{
+                    borderColor: "transparent",
+                    p: 0,
+                    // @ts-ignore TODO: fix type error upstream. This `size` prop should be derived from `Button` due to the `asChild` prop. Works at runtime.
+                    size: "sm",
+                  }}
+                  // @ts-ignore TODO: omit `value` upstream. The value is derived internally.
+                  swatchProps={{
+                    h: "full",
+                    w: "full",
+                    borderRadius: "sm",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    children: (
+                      <Icon
+                        src={HiOutlineEyeDropper}
+                        color="background.default"
+                        h={5}
+                        w={5}
+                      />
+                    ),
+                  }}
+                />
+              </Stack>
+            );
+          })}
 
         {/* TODO: figure out if this needs to be dynamic. Used as placeholder to fill the grid */}
         <Box
