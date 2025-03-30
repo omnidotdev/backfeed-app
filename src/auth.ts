13e0fe7554b8e74ca3dce487417cb98dba1dcbb2
Brian Cooper <brian@brian-cooper.com>
2025-03-29 20:16:26 -0500

    chore: format

--- src/auth.ts
+++ src/auth.ts
@@ -60,7 +60,7 @@ interface UpdatedTokens {
 const sdk = ({ headers }: { headers?: HeadersInit } = {}) => {
   const graphqlClient = new GraphQLClient(
     process.env.NEXT_PUBLIC_API_BASE_URL!,
-    { headers },
+    { headers }
   );
 
   return getSdk(graphqlClient);
@@ -136,7 +136,7 @@ export const { handlers, auth } = NextAuth({
               grant_type: "refresh_token",
               refresh_token: token.refresh_token,
             }),
-          },
+          }
         );
 
         const tokensOrError = await response.json();
