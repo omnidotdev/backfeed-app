3713312e02e1ae130590ebee1ce9487bbb06a26f
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-03-31 17:18:51 -0500

    fix: remove ref to getAuthSession

--- src/lib/flags/identity/subscription.identity.ts
+++ src/lib/flags/identity/subscription.identity.ts
@@ -1,14 +1,14 @@
 import { dedupe } from "flags/next";
 
+import { auth } from "auth";
 import { getSubscription } from "lib/actions";
-import { getAuthSession } from "lib/util";
 
 /**
  * Deduplicated identity helper to fetch the subscription of the current user for feature flags.
  */
 const dedupeSubscription = dedupe(async () => {
   try {
-    const session = await getAuthSession();
+    const session = await auth();
     const subscription = await getSubscription(session?.user?.hidraId!);
 
     return subscription;
