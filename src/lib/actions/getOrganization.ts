a09b972ea1902063ae8177cb44111b31d62fa399
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-04-16 13:28:46 -0500

    docs(actions): add JSDoc to getOrganization and getProject

--- src/lib/actions/getOrganization.ts
+++ src/lib/actions/getOrganization.ts
@@ -5,22 +5,28 @@ import { getAuthSession } from "lib/util";
 
 import type { Organization } from "generated/graphql";
 
-interface ProjectOptions {
+interface OrganizationOptions {
+  /** Organization slug. */
   organizationSlug: Organization["slug"];
 }
 
-const getOrganization = cache(async ({ organizationSlug }: ProjectOptions) => {
-  const session = await getAuthSession();
+/**
+ * Helper function to fetch an organization's details. Cached for deduping requests.
+ */
+const getOrganization = cache(
+  async ({ organizationSlug }: OrganizationOptions) => {
+    const session = await getAuthSession();
 
-  if (!session) return null;
+    if (!session) return null;
 
-  const sdk = getSdk({ session });
+    const sdk = getSdk({ session });
 
-  const { organizationBySlug: organization } = await sdk.Organization({
-    slug: organizationSlug,
-  });
+    const { organizationBySlug: organization } = await sdk.Organization({
+      slug: organizationSlug,
+    });
 
-  return organization;
-});
+    return organization;
+  }
+);
 
 export default getOrganization;
