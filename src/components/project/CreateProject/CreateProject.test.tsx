d88d0d7d52734a915e07cabb9e0eb9b52fb3f490
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-04-16 16:56:47 -0500

    test: add unit test placeholders for create org and create project

---
+++ src/components/project/CreateProject/CreateProject.test.tsx
@@ -0,0 +1,40 @@
+import { beforeEach, describe, it } from "bun:test";
+
+import { render } from "test/unit/util";
+import CreateProject from "./CreateProject";
+
+// TODO fill out the `todo` tests and update renders/tests to include `organizationSlug` prop conditionally
+describe("create project", () => {
+  describe("no subscription", () => {
+    beforeEach(() => {
+      render(<CreateProject isBasicTier={false} isTeamTier={false} />);
+    });
+
+    it.todo("user can not create a project", () => {});
+  });
+
+  describe("basic tier subscription", () => {
+    beforeEach(() => {
+      render(<CreateProject isBasicTier={true} isTeamTier={false} />);
+    });
+
+    it.todo("user (non admin) can not create a single project", () => {});
+
+    it.todo("user (admin) can create single project", () => {});
+
+    it.todo(
+      "user (admin) can not create more than 3 projects for the same organization",
+      () => {}
+    );
+  });
+
+  describe("team tier subscription", () => {
+    beforeEach(() => {
+      render(<CreateProject isBasicTier={true} isTeamTier={true} />);
+    });
+
+    it.todo("user (non admin) can not create a single project", () => {});
+
+    it.todo("user (admin) can create more than 3 projects", () => {});
+  });
+});
