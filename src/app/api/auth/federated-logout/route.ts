9b4e502b1dc4a9da347dccc1eb2dd620515e7e1a
Brian Cooper <brian@brian-cooper.com>
2025-04-16 22:14:46 -0500

    chore(auth): update env vars

--- src/app/api/auth/federated-logout/route.ts
+++ src/app/api/auth/federated-logout/route.ts
@@ -1,4 +1,5 @@
 import { auth } from "auth";
+import { AUTH_CLIENT_ID, AUTH_ISSUER } from "lib/config";
 
 /**
  * Federated logout route handler. This route is used to log the user out of the IDP session.
@@ -9,21 +10,18 @@ export const POST = async () => {
 
     if (session) {
       // TODO update, no longer using Keycloak. Determine proper `end_session_endpoint` for Better Auth
-      await fetch(
-        `${process.env.AUTH_KEYCLOAK_ISSUER}/protocol/openid-connect/logout`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-            Authorization: `Bearer ${session.accessToken}`,
-          },
-          body: new URLSearchParams({
-            client_id: process.env.AUTH_KEYCLOAK_ID!,
-            client_secret: process.env.AUTH_KEYCLOAK_SECRET!,
-            refresh_token: session.refreshToken,
-          }),
+      await fetch(`${AUTH_ISSUER}/protocol/openid-connect/logout`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization: `Bearer ${session.accessToken}`,
         },
-      );
+        body: new URLSearchParams({
+          client_id: AUTH_CLIENT_ID!,
+          client_secret: AUTH_CLIENT_ID!,
+          refresh_token: session.refreshToken,
+        }),
+      });
     }
 
     return Response.json(
