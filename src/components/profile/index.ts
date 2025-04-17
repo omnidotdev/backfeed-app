e4f893a0696efb4e49482ab6649261d0cf01e8b8
Beau Hawkinson <72956780+Twonarly1@users.noreply.github.com>
2025-04-16 19:15:36 -0500

    chore: update barrell file export alphabetically

--- src/components/profile/index.ts
+++ src/components/profile/index.ts
@@ -1,6 +1,6 @@
+export { default as Account } from "./Account/Account";
 export { default as InvitationMenu } from "./InvitationMenu/InvitationMenu";
 export { default as ManagementNavigation } from "./ManagementNavigation/ManagementNavigation";
 export { default as ManagementSidebar } from "./ManagementSidebar/ManagementSidebar";
 export { default as OrganizationInvites } from "./OrganizationInvites/OrganizationInvites";
 export { default as Subscription } from "./Subscription/Subscription";
-export { default as Account } from "./Account/Account";
