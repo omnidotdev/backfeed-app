09900d7037e0e99b42007d220b8ecfa42dec6a49
Beau Hawkinson <72956780+Twonarly1@users.noreply.github.com>
2025-04-22 12:28:33 -0500

    Update src/components/layout/AccountInformation/AccountInformation.tsx
    Co-authored-by: hobbescodes <87732294+hobbescodes@users.noreply.github.com>

--- src/components/layout/AccountInformation/AccountInformation.tsx
+++ src/components/layout/AccountInformation/AccountInformation.tsx
@@ -17,7 +17,6 @@ import {
 import { signOut } from "next-auth/react";
 import { redirect, useRouter } from "next/navigation";
 import { FiLogOut, FiUser } from "react-icons/fi";
-
 import { HiChevronUpDown } from "react-icons/hi2";
 import { useRef, useState } from "react";
 import { useOnClickOutside } from "usehooks-ts";
