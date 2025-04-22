8684d18df02f788d5e52332b718f2d2d52a619e0
Beau Hawkinson <72956780+Twonarly1@users.noreply.github.com>
2025-04-22 13:04:25 -0500

    chore: implement early returns for loading and errors on subscription page

--- src/components/profile/Subscription/Subscription.tsx
+++ src/components/profile/Subscription/Subscription.tsx
@@ -41,74 +41,73 @@ const Subscription = ({ customer }: Props) => {
     enabled: customer.status !== "rejected",
   });
 
+  if (isLoading) return <Skeleton h={18} m={4} />;
+
+  if (customer.status === "rejected" || error)
+    return (
+      <Stack>
+        {app.profileSubscriptionPage.table.emptyState.label}
+        <Link href="/pricing">
+          <Button>
+            {app.profileSubscriptionPage.table.actions.subscribe.label}
+          </Button>
+        </Link>
+      </Stack>
+    );
+
   return (
-    <>
-      {isLoading ? (
-        <Skeleton h={18} m={4} />
-      ) : customer.status === "rejected" || error ? (
-        <Stack>
-          {app.profileSubscriptionPage.table.emptyState.label}
-          <Link href="/pricing">
-            <Button>
-              {app.profileSubscriptionPage.table.actions.subscribe.label}
-            </Button>
-          </Link>
-        </Stack>
-      ) : (
-        <Flex w="100%" overflowX="auto">
-          <Table
-            headerContent={
-              <TableRow bgColor="transparent">
-                {Object.values(app.profileSubscriptionPage.table.headers).map(
-                  (header) => (
-                    <TableCell key={header} fontWeight="bold">
-                      {header}
-                    </TableCell>
-                  ),
-                )}
-              </TableRow>
-            }
-            textWrap="nowrap"
-          >
-            <TableRow fontSize={{ base: "sm", md: "lg" }} bgColor="transparent">
-              <TableCell>{subscription?.product.name}</TableCell>
+    <Flex w="100%" overflowX="auto">
+      <Table
+        headerContent={
+          <TableRow bgColor="transparent">
+            {Object.values(app.profileSubscriptionPage.table.headers).map(
+              (header) => (
+                <TableCell key={header} fontWeight="bold">
+                  {header}
+                </TableCell>
+              ),
+            )}
+          </TableRow>
+        }
+        textWrap="nowrap"
+      >
+        <TableRow fontSize={{ base: "sm", md: "lg" }} bgColor="transparent">
+          <TableCell>{subscription?.product.name}</TableCell>
 
-              <TableCell>
-                <HStack>
-                  <Circle size={2} bgColor="brand.tertiary" />
+          <TableCell>
+            <HStack>
+              <Circle size={2} bgColor="brand.tertiary" />
 
-                  {capitalizeFirstLetter(subscription?.status)}
-                </HStack>
-              </TableCell>
+              {capitalizeFirstLetter(subscription?.status)}
+            </HStack>
+          </TableCell>
 
-              <TableCell>
-                ${(subscription?.amount ?? 0) / 100}/
-                {subscription?.recurringInterval}
-              </TableCell>
+          <TableCell>
+            ${(subscription?.amount ?? 0) / 100}/
+            {subscription?.recurringInterval}
+          </TableCell>
 
-              <TableCell>
-                <Flex justify="flex-end">
-                  {/* NB: `SigilLink` is used to avoid initial CORS issues on external domain prefetching for the redirect */}
-                  <SigilLink
-                    href={`/api/customer/portal?customerId=${customer.value.id}`}
-                    textDecoration="none"
-                  >
-                    <Button>
-                      <Icon src={LuSettings} h={4} w={4} />
+          <TableCell>
+            <Flex justify="flex-end">
+              {/* NB: `SigilLink` is used to avoid initial CORS issues on external domain prefetching for the redirect */}
+              <SigilLink
+                href={`/api/customer/portal?customerId=${customer.value.id}`}
+                textDecoration="none"
+              >
+                <Button>
+                  <Icon src={LuSettings} h={4} w={4} />
 
-                      {
-                        app.profileSubscriptionPage.table.actions
-                          .manageSubscription.label
-                      }
-                    </Button>
-                  </SigilLink>
-                </Flex>
-              </TableCell>
-            </TableRow>
-          </Table>
-        </Flex>
-      )}
-    </>
+                  {
+                    app.profileSubscriptionPage.table.actions.manageSubscription
+                      .label
+                  }
+                </Button>
+              </SigilLink>
+            </Flex>
+          </TableCell>
+        </TableRow>
+      </Table>
+    </Flex>
   );
 };
 
