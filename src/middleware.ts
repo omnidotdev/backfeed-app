a10003c4e6c777b9b11f0c00837038ab9f28607c
Beau Hawkinson <72956780+Twonarly1@users.noreply.github.com>
2025-04-22 07:48:30 -0500

    Update src/middleware.ts
    Co-authored-by: hobbescodes <87732294+hobbescodes@users.noreply.github.com>

--- src/middleware.ts
+++ src/middleware.ts
@@ -74,7 +74,7 @@ export const middleware = auth(async (request) => {
   // Redirect user to their profile page upon successful checkout (or force redirect when trying to access confirmation route)
   if (request.nextUrl.pathname.startsWith("/confirmation")) {
     return NextResponse.redirect(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/profile/${request.auth.user?.rowId}/subscription`,
+      `${process.env.NEXT_PUBLIC_BASE_URL}/profile/${request.auth.user?.hidraId}/subscription`,
     );
   }
 
