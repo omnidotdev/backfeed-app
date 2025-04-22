a6f0651f61aabe3ced30400d2cc7f1013d50e3a1
Beau Hawkinson <72956780+Twonarly1@users.noreply.github.com>
2025-04-22 12:25:06 -0500

    chore: remove hydration boundary and queryClient from invitation page

--- src/app/profile/[userId]/invitations/page.tsx
+++ src/app/profile/[userId]/invitations/page.tsx
@@ -1,11 +1,9 @@
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import { notFound } from "next/navigation";
 
 import { auth } from "auth";
 import { Page } from "components/layout";
 import { OrganizationInvites } from "components/profile";
 import { app } from "lib/config";
-import { getQueryClient } from "lib/util";
 
 /**
  * Profile invitations page.
@@ -15,23 +13,19 @@ const ProfileInvitationsPage = async () => {
 
   if (!session) notFound();
 
-  const queryClient = getQueryClient();
-
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <Page
-        metadata={{
-          title: app.profileInvitationsPage.breadcrumb,
-        }}
-        header={{
-          title: app.profileInvitationsPage.breadcrumb,
-          description: app.profileInvitationsPage.description,
-        }}
-        pt={0}
-      >
-        <OrganizationInvites email={session?.user?.email!} />
-      </Page>
-    </HydrationBoundary>
+    <Page
+      metadata={{
+        title: app.profileInvitationsPage.breadcrumb,
+      }}
+      header={{
+        title: app.profileInvitationsPage.breadcrumb,
+        description: app.profileInvitationsPage.description,
+      }}
+      pt={0}
+    >
+      <OrganizationInvites email={session?.user?.email!} />
+    </Page>
   );
 };
 
