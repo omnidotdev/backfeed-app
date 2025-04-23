b3d2fce89541379a41b2f10224d084cdf47d183a
Brian Cooper <brian@brian-cooper.com>
2025-04-23 14:12:16 -0500

    chore(auth): add credit to headers workaround

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 /**
  * @file Auth route handlers.
+ * This workaround is necessary to properly set up callback URLs and other configuration. This solution is from https://github.com/nextauthjs/next-auth/issues/10928#issuecomment-2144241314
  */
 
 import { handlers } from "auth";
