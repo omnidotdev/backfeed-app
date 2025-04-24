fbe3ce64124c3dffdd1a07f773887391cfc9a912
Brian Cooper <brian@brian-cooper.com>
2025-04-24 00:07:05 -0500

    chore(auth): set provider-level redirect proxy URL

--- src/auth.ts
+++ src/auth.ts
@@ -115,6 +115,8 @@ export const { handlers, auth } = NextAuth({
       issuer: AUTH_ISSUER,
       clientId: AUTH_CLIENT_ID,
       clientSecret: AUTH_CLIENT_SECRET,
+      redirectProxyUrl:
+        "https://backfeed-app-prerelease.up.railway.app/api/auth",
       // TODO also add `nonce` check, currently `OperationProcessingError: JWT "nonce" (nonce) claim missing`
       // PKCE protects against authorization code interception
       // State parameter prevents CSRF attacks
