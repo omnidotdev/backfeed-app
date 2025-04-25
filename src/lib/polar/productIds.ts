5766079860e752b5d331a0eb15cf8be40d035cbb
Brian Cooper <brian@brian-cooper.com>
2025-04-24 20:14:27 -0500

    refactor(polar): type product IDs (UUID), add JSDoc, improve readability

--- src/lib/polar/productIds.ts
+++ src/lib/polar/productIds.ts
@@ -1,16 +1,47 @@
-const BACKFEED_PRODUCT_IDS: string[] = [
-  "9fdf9d78-f4f7-4222-93ed-55ae60d996e4", // Backfeed Basic (Monthly) sandbox
-  "9a135ea6-5e7c-448b-8070-a5534ae72d93", // Backfeed Basic (Yearly) sandbox
-  "51eaf279-48b4-422a-939f-046cb299655d", // Backfeed Team (Monthly) sandbox
-  "2479fd7f-03b3-4965-9887-e701e8e18e14", // Backfeed Team (Yearly) sandbox
-  "3776b994-1c73-4869-afef-f34a2ca68181", // Backfeed Enterprise (Monthly) sandbox
-  "48d27c97-50da-441b-ba44-d90b8c2d60b3", // Backfeed Enterprise (Yearly) sandbox
-  "768e1aee-fc38-46fa-91d9-1c15b71375dd", // Backfeed Basic (Monthly) prod
-  "a35183cc-5223-4e64-852c-a291a6f52222", // Backfeed Basic (Yearly) prod
-  "b1f48ae5-0de4-416e-9b16-39b3f01372b6", // Backfeed Team (Monthly) prod
-  "39e6bf3d-0aef-4fe0-abdb-243af14b010f", // Backfeed Team (Yearly) prod
-  "7e2bd7d7-6a43-4c0d-8545-c59d2fabd598", // Backfeed Enterprise (Monthly) prod
-  "51906ba1-b6a9-43c9-83ee-f2fe399a792f", // Backfeed Enterprise (Yearly) prod
+import type { UUID } from "node:crypto";
+
+/**
+ * Polar sandbox Product IDs.
+ */
+const sandboxProductIds: UUID[] = [
+  // Backfeed Basic (Monthly)
+  "9fdf9d78-f4f7-4222-93ed-55ae60d996e4",
+  // Backfeed Basic (Yearly)
+  "9a135ea6-5e7c-448b-8070-a5534ae72d93",
+  // Backfeed Team (Monthly)
+  "51eaf279-48b4-422a-939f-046cb299655d",
+  // Backfeed Team (Yearly)
+  "2479fd7f-03b3-4965-9887-e701e8e18e14",
+  // Backfeed Enterprise (Monthly)
+  "3776b994-1c73-4869-afef-f34a2ca68181",
+  // Backfeed Enterprise (Yearly)
+  "48d27c97-50da-441b-ba44-d90b8c2d60b3",
+] as const;
+
+/**
+ * Polar production product IDs.
+ */
+const productionProductIds: UUID[] = [
+  // Backfeed Basic (Monthly)
+  "768e1aee-fc38-46fa-91d9-1c15b71375dd",
+  // Backfeed Basic (Yearly)
+  "a35183cc-5223-4e64-852c-a291a6f52222",
+  // Backfeed Team (Monthly)
+  "b1f48ae5-0de4-416e-9b16-39b3f01372b6",
+  // Backfeed Team (Yearly)
+  "39e6bf3d-0aef-4fe0-abdb-243af14b010f",
+  // Backfeed Enterprise (Monthly)
+  "7e2bd7d7-6a43-4c0d-8545-c59d2fabd598",
+  // Backfeed Enterprise (Yearly)
+  "51906ba1-b6a9-43c9-83ee-f2fe399a792f",
+] as const;
+
+/**
+ * Polar product IDs.
+ */
+const BACKFEED_PRODUCT_IDS: UUID[] = [
+  ...sandboxProductIds,
+  ...productionProductIds,
 ] as const;
 
 export default BACKFEED_PRODUCT_IDS;
