c329ad5ea99d9fa796501b986c66db17506fbeb4
Brian Cooper <brian@brian-cooper.com>
2025-04-24 20:30:01 -0500

    refactor(env): rename Polar sandbox switch

--- src/lib/polar/polar.ts
+++ src/lib/polar/polar.ts
@@ -1,13 +1,13 @@
 import { Polar } from "@polar-sh/sdk";
 
-import { SANDBOX } from "lib/config";
+import { ENABLE_POLAR_SANDBOX } from "lib/config";
 
 /**
  * Polar SDK client.
  */
 const polar = new Polar({
   accessToken: process.env.POLAR_ACCESS_TOKEN!,
-  server: SANDBOX ? "sandbox" : "production",
+  server: ENABLE_POLAR_SANDBOX ? "sandbox" : "production",
 });
 
 export default polar;
