99bec0788e46fbb0e301d800ddd98be22f6770de
Brian Cooper <brian@brian-cooper.com>
2025-04-24 20:54:44 -0500

    chore(auth): set raw Next Auth handlers

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,31 +1,6 @@
-/**
- * @file Auth route handlers.
- * This workaround is necessary to properly set up callback URLs and other configuration. This solution is from https://github.com/nextauthjs/next-auth/issues/10928#issuecomment-2144241314
- */
-
 import { handlers } from "auth";
-import { NextRequest } from "next/server";
 
-const reqWithTrustedOrigin = (req: NextRequest): NextRequest => {
-  if (process.env.AUTH_TRUST_HOST !== "true") return req;
-
-  const proto = req.headers.get("x-forwarded-proto");
-  const host = req.headers.get("x-forwarded-host");
-
-  if (!proto || !host) {
-    console.warn("Missing x-forwarded-proto or x-forwarded-host headers.");
-    return req;
-  }
-
-  const envOrigin = `${proto}://${host}`;
-
-  const { href, origin } = req.nextUrl;
-
-  return new NextRequest(href.replace(origin, envOrigin), req);
-};
-
-export const GET = (req: NextRequest) =>
-  handlers.GET(reqWithTrustedOrigin(req));
-
-export const POST = (req: NextRequest) =>
-  handlers.POST(reqWithTrustedOrigin(req));
+/**
+ * Auth route handlers.
+ */
+export const { GET, POST } = handlers;
