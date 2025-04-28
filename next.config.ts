16dc688f52c498465463f74c8dd6a8a985433660
Brian Cooper <brian@brian-cooper.com>
2025-04-28 11:16:00 -0500

    chore(next): disable security headers, set `Access-Control-Allow-Headers`

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ import type { NextConfig } from "next";
  * CORS headers.
  */
 const corsHeaders = [
+  {
+    key: "Access-Control-Allow-Headers",
+    value: "Content-Type",
+  },
   { key: "Access-Control-Allow-Credentials", value: "true" },
   {
     key: "Access-Control-Allow-Origin",
@@ -21,60 +25,60 @@ const nextConfig: NextConfig = {
       source: "/(.*?)",
       headers: [
         // prefetch DNS records in browse for faster loading
-        {
-          key: "X-DNS-Prefetch-Control",
-          value: "on",
-        },
-        // force HTTPS usage and allow preload list inclusion
-        {
-          key: "Strict-Transport-Security",
-          value: "max-age=63072000; includeSubDomains; preload",
-        },
-        // prevent the site from being embedded in iframes from other domains (clickjacking protection)
-        {
-          key: "Content-Security-Policy",
-          value: "frame-ancestors 'self'",
-        },
-        // prevent the browser from guessing (sniffing) the MIME type
-        {
-          key: "X-Content-Type-Options",
-          value: "nosniff",
-        },
-        // enable legacy XSS protection mode in older browsers; 1 enables XSS protection and block mode prevents the page from loading if an attack is detected
-        {
-          key: "X-XSS-Protection",
-          value: "1; mode=block",
-        },
-        // block site from being framed by other origins (legacy equivalent of CSP `frame-ancestors`)
-        {
-          key: "X-Frame-Options",
-          value: "SAMEORIGIN",
-        },
-        // restrict how much referrer information is sent with requests to other sites
-        {
-          key: "Referrer-Policy",
-          value: "strict-origin-when-cross-origin",
-        },
-        // opt the origin into its own memory/process context (Spectre defense)
-        {
-          key: "Origin-Agent-Cluster",
-          value: "?1",
-        },
-        // prevent other origins from loading resources unless allowed
-        {
-          key: "Cross-Origin-Resource-Policy",
-          value: "same-origin",
-        },
-        // TODO discuss which permissions are needed/if they are needed at all
-        {
-          key: "Permissions-Policy",
-          value: "geolocation=(), microphone=(), camera=(), fullscreen=()",
-        },
-        // block legacy Adobe plugins (i.e. Flash) from loading cross-domain data
-        {
-          key: "X-Permitted-Cross-Domain-Policies",
-          value: "none",
-        },
+        // {
+        //   key: "X-DNS-Prefetch-Control",
+        //   value: "on",
+        // },
+        // // force HTTPS usage and allow preload list inclusion
+        // {
+        //   key: "Strict-Transport-Security",
+        //   value: "max-age=63072000; includeSubDomains; preload",
+        // },
+        // // prevent the site from being embedded in iframes from other domains (clickjacking protection)
+        // {
+        //   key: "Content-Security-Policy",
+        //   value: "frame-ancestors 'self'",
+        // },
+        // // prevent the browser from guessing (sniffing) the MIME type
+        // {
+        //   key: "X-Content-Type-Options",
+        //   value: "nosniff",
+        // },
+        // // enable legacy XSS protection mode in older browsers; 1 enables XSS protection and block mode prevents the page from loading if an attack is detected
+        // {
+        //   key: "X-XSS-Protection",
+        //   value: "1; mode=block",
+        // },
+        // // block site from being framed by other origins (legacy equivalent of CSP `frame-ancestors`)
+        // {
+        //   key: "X-Frame-Options",
+        //   value: "SAMEORIGIN",
+        // },
+        // // restrict how much referrer information is sent with requests to other sites
+        // {
+        //   key: "Referrer-Policy",
+        //   value: "strict-origin-when-cross-origin",
+        // },
+        // // opt the origin into its own memory/process context (Spectre defense)
+        // {
+        //   key: "Origin-Agent-Cluster",
+        //   value: "?1",
+        // },
+        // // prevent other origins from loading resources unless allowed
+        // {
+        //   key: "Cross-Origin-Resource-Policy",
+        //   value: "same-origin",
+        // },
+        // // TODO discuss which permissions are needed/if they are needed at all
+        // {
+        //   key: "Permissions-Policy",
+        //   value: "geolocation=(), microphone=(), camera=(), fullscreen=()",
+        // },
+        // // block legacy Adobe plugins (i.e. Flash) from loading cross-domain data
+        // {
+        //   key: "X-Permitted-Cross-Domain-Policies",
+        //   value: "none",
+        // },
         // set CORS headers for identity provider
         ...corsHeaders,
       ],
