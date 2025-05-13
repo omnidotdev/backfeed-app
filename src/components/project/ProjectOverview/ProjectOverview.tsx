d774802cbad6a9135f2a68be8a67375d68d91970
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-05-12 22:04:29 -0500

    docs(project-overview): add JSDoc

--- src/components/project/ProjectOverview/ProjectOverview.tsx
+++ src/components/project/ProjectOverview/ProjectOverview.tsx
@@ -21,6 +21,9 @@ interface Props {
   projectId: Project["rowId"];
 }
 
+/**
+ * Project overview.
+ */
 const ProjectOverview = ({ user, projectId }: Props) => {
   // TODO: look into optimistic updates. Unnecessary for now, but would be nice for synchronous feedback with the details component. See: https://github.com/omnidotdev/backfeed-app/pull/58#issuecomment-2593070248 for more context.
   const { data, isLoading, isError } = useProjectMetricsQuery(
