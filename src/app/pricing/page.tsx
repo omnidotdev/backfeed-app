cb2d51d89ee5cf11a8ba5b65a483baaf6e539d0e
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-11-10 09:20:31 -0600

    refactor(pricing): remove redirect based on active subscription count

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -32,18 +32,7 @@ const PricingPage = async () => {
 
   // TODO: Odd cases where session cookie is not removed after refresh token expires. Believe it to be that this route is not captured by the middleware and therefore the cookie is not removed. Fix this in order to remove check for `session.error`.
   if (session && !session.error) {
-    // NB: `allSettled` is used to handle API errors, but take action on the results (i.e. replaces try/catch)
-    const [customer] = await Promise.allSettled([
-      polar.customers.getStateExternal({
-        externalId: session.user.hidraId!,
-      }),
-    ]);
-
-    if (
-      customer.status !== "rejected" &&
-      customer.value.activeSubscriptions.length
-    )
-      redirect(`/profile/${session.user.hidraId}/subscriptions`);
+    redirect(`/profile/${session.user.hidraId}/subscriptions`);
   }
 
   return <PricingOverview products={products} />;
