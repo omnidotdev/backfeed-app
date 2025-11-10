cede2ef0a905acf1f6de034c7d7cf8063f1d102e
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-11-10 10:11:17 -0600

    fix(get-subscriptions): filter based on backfeed product IDs

--- src/lib/actions/getSubscriptions.ts
+++ src/lib/actions/getSubscriptions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { getCustomer, getProduct } from "lib/actions";
+import { BACKFEED_PRODUCT_IDS } from "lib/polar";
 
 /**
  * Server action to get details about a user's active subscriptions.
@@ -8,21 +9,28 @@ import { getCustomer, getProduct } from "lib/actions";
 const getSubscriptions = async (userId: string) => {
   const customer = await getCustomer(userId);
 
-  if (!customer.activeSubscriptions.length) {
-    throw new Error("No active subscriptions found");
+  if (
+    // TODO: determine if there is a better way to do this. Since the Polar organization scope is beyond Backfeed, we have to apply additional checks
+    !customer.activeSubscriptions.filter((sub) =>
+      BACKFEED_PRODUCT_IDS!.includes(sub.productId),
+    ).length
+  ) {
+    throw new Error("No active Backfeed subscriptions found");
   }
 
   const subscriptionsWithProduct = await Promise.all(
-    customer.activeSubscriptions.map(async (sub) => {
-      const product = await getProduct(sub.productId);
+    customer.activeSubscriptions
+      .filter((sub) => BACKFEED_PRODUCT_IDS!.includes(sub.productId))
+      .map(async (sub) => {
+        const product = await getProduct(sub.productId);
 
-      const { productId, ...rest } = sub;
+        const { productId, ...rest } = sub;
 
-      return {
-        ...rest,
-        product,
-      };
-    }),
+        return {
+          ...rest,
+          product,
+        };
+      }),
   );
 
   return subscriptionsWithProduct;
