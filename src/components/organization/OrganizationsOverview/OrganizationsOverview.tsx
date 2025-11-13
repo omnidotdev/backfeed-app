ccd4b7f5c9becf81ab5bb12d99e9fcfb8e0db3c4
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-11-12 18:03:31 -0600

    Revert "chore: remove TODO"
    This reverts commit 23b1f9f0fdc71ee1c950853be2d3ae377fcb92a7.

--- src/components/organization/OrganizationsOverview/OrganizationsOverview.tsx
+++ src/components/organization/OrganizationsOverview/OrganizationsOverview.tsx
@@ -78,6 +78,7 @@ const OrganizationsOverview = ({ user }: Props) => {
         cta: [
           {
             label: app.organizationsPage.header.cta.newOrganization.label,
+            // TODO: get Sigil Icon component working and update accordingly. Context: https://github.com/omnidotdev/backfeed-app/pull/44#discussion_r1897974331
             icon: <Icon src={LuCirclePlus} />,
             dialogType: DialogType.CreateOrganization,
             disabled: !tierRestrictions?.canCreateOrganizations,
