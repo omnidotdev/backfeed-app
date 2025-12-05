023a9163f1f2ed9d60ae58c7b94b1f509b94c767
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-12-05 15:27:31 -0600

    refactor(invitations): ensure invitations data in loader

--- src/routes/_auth/organizations/$organizationSlug/_layout/_manage/invitations.tsx
+++ src/routes/_auth/organizations/$organizationSlug/_layout/_manage/invitations.tsx
@@ -8,14 +8,17 @@ import Invitations from "@/components/organization/Invitations";
 import InviteMember from "@/components/organization/InviteMember";
 import { Role } from "@/generated/graphql";
 import app from "@/lib/config/app.config";
+import { invitationsOptions } from "@/lib/options/invitations";
 import { organizationOptions } from "@/lib/options/organizations";
 import { DialogType } from "@/lib/store/useDialogStore";
 
 export const Route = createFileRoute(
   "/_auth/organizations/$organizationSlug/_layout/_manage/invitations",
 )({
-  loader: async ({ context: { role } }) => {
+  loader: async ({ context: { queryClient, role, organizationId } }) => {
     if (!role || role === Role.Member) throw notFound();
+
+    await queryClient.ensureQueryData(invitationsOptions({ organizationId }));
   },
   component: OrganizationInvitationsPage,
 });
