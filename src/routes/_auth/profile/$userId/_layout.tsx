294349843093a090f7da007ba3971d8201224e5f
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-12-06 00:40:20 -0600

    refactor(invitations): add render logic for user invitations page

--- src/routes/_auth/profile/$userId/_layout.tsx
+++ src/routes/_auth/profile/$userId/_layout.tsx
@@ -1,13 +1,19 @@
-import { Outlet, createFileRoute } from "@tanstack/react-router";
+import { Outlet, createFileRoute, redirect } from "@tanstack/react-router";
 
 import ProfileSidebar from "@/components/profile/ProfileSidebar";
 import { userOptions } from "@/lib/options/users";
 
 export const Route = createFileRoute("/_auth/profile/$userId/_layout")({
-  beforeLoad: async ({ context: { queryClient }, params: { userId } }) => {
+  beforeLoad: async ({
+    context: { session, queryClient },
+    params: { userId },
+  }) => {
     const { userByHidraId } = await queryClient.ensureQueryData(
       userOptions({ hidraId: userId }),
     );
+
+    if (!userByHidraId || userByHidraId.rowId !== session?.user?.rowId)
+      throw redirect({ to: "/" });
 
     return { user: userByHidraId };
   },
--- src/routes/_auth/profile/$userId/_layout/invitations.tsx
+++ src/routes/_auth/profile/$userId/_layout/invitations.tsx
@@ -1,11 +1,31 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import Page from "@/components/layout/Page";
+import OrganizationInvites from "@/components/profile/OrganizationInvites";
+import app from "@/lib/config/app.config";
+import { invitationsOptions } from "@/lib/options/invitations";
+
 export const Route = createFileRoute(
   "/_auth/profile/$userId/_layout/invitations",
 )({
+  loader: async ({ context: { queryClient, user } }) => {
+    await queryClient.ensureQueryData(
+      invitationsOptions({ email: user?.email! }),
+    );
+  },
   component: UserInvitationsPage,
 });
 
 function UserInvitationsPage() {
-  return <div>Hello "/_auth/_profile/$userId/invitations"!</div>;
+  return (
+    <Page
+      header={{
+        title: app.profileInvitationsPage.breadcrumb,
+        description: app.profileInvitationsPage.description,
+      }}
+      pt={0}
+    >
+      <OrganizationInvites />
+    </Page>
+  );
 }
