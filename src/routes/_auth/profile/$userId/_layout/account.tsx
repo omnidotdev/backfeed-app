96a04db47d5410b9d7a4bb42874094ca8d72f83d
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-12-06 00:28:14 -0600

    refactor(account): remove component extraction

--- src/routes/_auth/profile/$userId/_layout/account.tsx
+++ src/routes/_auth/profile/$userId/_layout/account.tsx
@@ -1,17 +1,47 @@
-import { Icon } from "@omnidev/sigil";
+import { Button, Flex, Icon, Input, Label, Stack } from "@omnidev/sigil";
 import { createFileRoute } from "@tanstack/react-router";
+import { useMemo, useState } from "react";
 import { FaRegEdit } from "react-icons/fa";
+import { IoEyeOffOutline, IoEyeOutline } from "react-icons/io5";
 
 import Page from "@/components/layout/Page";
-import Account from "@/components/profile/Account";
 import app from "@/lib/config/app.config";
 import { AUTH_ISSUER } from "@/lib/config/env.config";
+
+import type { InputProps } from "@omnidev/sigil";
 
 export const Route = createFileRoute("/_auth/profile/$userId/_layout/account")({
   component: UserAccountPage,
 });
 
 function UserAccountPage() {
+  const { user } = Route.useRouteContext();
+
+  const [showEmail, setShowEmail] = useState(false);
+
+  const profileData = useMemo<InputProps[]>(
+    () => [
+      {
+        id: app.profileAccountPage.fields.username.label,
+        value: user?.username ?? "",
+      },
+      {
+        id: app.profileAccountPage.fields.firstName.label,
+        value: user?.firstName ?? "",
+      },
+      {
+        id: app.profileAccountPage.fields.lastName.label,
+        value: user?.lastName ?? "",
+      },
+      {
+        id: app.profileAccountPage.fields.email.label,
+        value: user?.email,
+        type: "password",
+      },
+    ],
+    [user],
+  );
+
   return (
     <Page
       header={{
@@ -30,7 +60,56 @@ function UserAccountPage() {
       }}
       pt={0}
     >
-      <Account />
+      <Stack gap={8} h="full" justifyContent="space-between">
+        <Stack gap={4}>
+          {profileData.map(({ id, value, type }) => {
+            const isEmail = id === app.profileAccountPage.fields.email.label;
+
+            return (
+              <Stack key={id} gap={1}>
+                <Label htmlFor={id}>{id}</Label>
+
+                <Flex position="relative" gap={4}>
+                  <Input
+                    disabled
+                    id={id}
+                    type={showEmail && isEmail ? "text" : type}
+                    value={value}
+                    opacity={1}
+                    borderColor="border.subtle"
+                    color="foreground.subtle"
+                  />
+
+                  {isEmail && (
+                    <Button
+                      type="button"
+                      variant="ghost"
+                      size="sm"
+                      pos="absolute"
+                      right={0.5}
+                      bottom={0.5}
+                      w="fit"
+                      _hover={{ opacity: "40%", bgColor: "transparent" }}
+                      onClick={() => setShowEmail((prev) => !prev)}
+                      aria-label={
+                        showEmail
+                          ? app.profileAccountPage.fields.email.ariaLabel.hide
+                          : app.profileAccountPage.fields.email.ariaLabel.show
+                      }
+                    >
+                      {showEmail ? (
+                        <IoEyeOutline aria-hidden="true" />
+                      ) : (
+                        <IoEyeOffOutline aria-hidden="true" />
+                      )}
+                    </Button>
+                  )}
+                </Flex>
+              </Stack>
+            );
+          })}
+        </Stack>
+      </Stack>
     </Page>
   );
 }
