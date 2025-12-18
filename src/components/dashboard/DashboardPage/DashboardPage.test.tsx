5c4af4b50d9f47acdef6362a8f48a51cb4846d41
Brian Cooper <brian@brian-cooper.com>
2025-12-18 01:19:14 -0800

    chore: ignore test type error

--- src/components/dashboard/DashboardPage/DashboardPage.test.tsx
+++ src/components/dashboard/DashboardPage/DashboardPage.test.tsx
@@ -14,6 +14,7 @@ describe("dashboard page", () => {
     render(
       <DashboardPage
         // TODO: mock user
+        // @ts-ignore TODO
         user={{}}
         oneWeekAgo={oneWeekAgo}
       />,
