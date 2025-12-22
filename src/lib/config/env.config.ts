a1f2fcb8a50b1b3af6bfb62b34e6b09745b330b6
Brian Cooper <brian@brian-cooper.com>
2025-12-21 17:36:40 -0800

    refactor: update env structure

--- src/lib/config/env.config.ts
+++ src/lib/config/env.config.ts
@@ -1,27 +1,28 @@
-// core
-const NODE_ENV = import.meta.env.NODE_ENV;
-export const BASE_URL = import.meta.env.VITE_BASE_URL;
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+/**
+ * Environment variables.
+ */
+export const {
+  // core
+  VITE_BASE_URL: BASE_URL,
+  VITE_API_BASE_URL: API_BASE_URL,
+  VITE_AUTH_BASE_URL: AUTH_BASE_URL,
+  // auth (server-side secrets)
+  AUTH_CLIENT_ID,
+  AUTH_CLIENT_SECRET,
+  // emails
+  VITE_FROM_EMAIL_ADDRESS: FROM_EMAIL_ADDRESS,
+  VITE_TO_EMAIL_ADDRESS: TO_EMAIL_ADDRESS,
+  // payment processing
+  VITE_STRIPE_PORTAL_CONFIG_ID: STRIPE_PORTAL_CONFIG_ID,
+} = { ...import.meta.env, ...process.env };
+
 export const API_GRAPHQL_URL = `${API_BASE_URL}/graphql`;
+export const AUTH_ISSUER_URL = `${AUTH_BASE_URL}/api/auth`;
 
-// simplified environment helpers
-export const isDevEnv = NODE_ENV === "development";
+// environment helpers
+export const isDevEnv = import.meta.env.DEV;
 // NB: `APP_ENV` is used instead of `NODE_ENV` because `next dev` shadows `NODE_ENV`, so even if `NODE_ENV=test` is injected into the environment, it will be overwritten to "development". See https://github.com/vercel/next.js/issues/17032
-const isTestEnv = NODE_ENV === "test";
-
-// auth
-export const AUTH_ISSUER = import.meta.env.VITE_AUTH_ISSUER;
-export const AUTH_CLIENT_ID = process.env.AUTH_CLIENT_ID;
-export const AUTH_CLIENT_SECRET = process.env.AUTH_CLIENT_SECRET;
-
-// emails
-export const FROM_EMAIL_ADDRESS = import.meta.env.VITE_FROM_EMAIL_ADDRESS;
-export const TO_EMAIL_ADDRESS = import.meta.env.VITE_TO_EMAIL_ADDRESS;
-
-// payment processing
-// customer portal session configuration: https://docs.stripe.com/api/customer_portal/sessions/create#create_portal_session-configuration
-export const STRIPE_PORTAL_CONFIG_ID = import.meta.env
-  .VITE_STRIPE_PORTAL_CONFIG_ID;
+const isTestEnv = import.meta.env.NODE_ENV === "test";
 
 // tests
 // enable mock service worker (https://mswjs.io/docs/integrations/browser#conditionally-enable-mocking), this is wrapped in case mocking requests and responses during development is desired
