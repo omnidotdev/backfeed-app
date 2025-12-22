8a2bc6ab9c4bbba32f3f1780ca7d0b9df78d3cbd
Brian Cooper <brian@brian-cooper.com>
2025-12-21 17:47:58 -0800

    chore: add TLS bypass env var, update sign in interface

--- src/lib/auth/signIn.ts
+++ src/lib/auth/signIn.ts
@@ -1,18 +1,20 @@
 import authClient from "@/lib/auth/authClient";
 
+interface SignInParams {
+  /** Redirect URL. */
+  redirectUrl: string;
+  // TODO: handle sign-up action if Better Auth supports it
+  // action?: "sign-up";
+}
+
 /**
  * Sign in with OAuth2 provider.
  */
-const signIn = async ({
-  redirectUrl,
-}: {
-  redirectUrl: string;
-  action?: "sign-up";
-}) => {
+const signIn = async ({ redirectUrl }: SignInParams) => {
   await authClient.signIn.oauth2({
+    // TODO env var/derive for the self-hosting fam
     providerId: "omni",
     callbackURL: redirectUrl,
-    // TODO: handle sign-up action if Better Auth supports it
   });
 };
 
