d9df414d9c05063bf97dc6ae7f3a59fb84fdeeb7
Brian Cooper <brian@brian-cooper.com>
2025-12-21 17:40:15 -0800

    build(vite): add JSDoc, convert export to default, remove proxy hack

--- vite.config.ts
+++ vite.config.ts
@@ -6,15 +6,17 @@ import { defineConfig, loadEnv } from "vite";
 import mkcert from "vite-plugin-mkcert";
 import tsConfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(({ command, mode }) => {
+/**
+ * Vite configuration.
+ * @see https://vite.dev/config
+ */
+const viteConfig = defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
     server: {
       port: 3000,
       host: "0.0.0.0",
-      // Seemingly needed as a workaround for `https`. See: https://github.com/TanStack/router/issues/4287#issuecomment-2954268842
-      proxy: {},
       cors: {
         origin: env.VITE_AUTH_BASE_URL,
         credentials: true,
@@ -31,3 +33,5 @@ export default defineConfig(({ command, mode }) => {
     ],
   };
 });
+
+export default viteConfig;
