fd3ac9af9333c21d3bbb5112b14bb694b1be3737
Brian Cooper <brian@brian-cooper.com>
2026-06-03 23:58:10 -0700

    refactor(layout): convert OrganizationSwitcher to shadcn Menu
    Replaces Sigil Menu with the shadcn Menu (ark) composition; adds popover theme
    tokens. onSelect/value/disabled/asChild behavior preserved; icons via
    react-icons directly.

--- src/components/layout/OrganizationSwitcher.tsx
+++ src/components/layout/OrganizationSwitcher.tsx
@@ -1,13 +1,15 @@
-import {
-  Button,
-  Icon,
-  Menu,
-  MenuItem,
-  MenuItemGroup,
-  MenuSeparator,
-} from "@omnidev/sigil";
 import { LuBuilding2, LuChevronDown, LuPlus, LuUser } from "react-icons/lu";
 
+import { Button } from "@/components/ui/button";
+import {
+  MenuContent,
+  MenuItem,
+  MenuItemGroup,
+  MenuPositioner,
+  MenuRoot,
+  MenuSeparator,
+  MenuTrigger,
+} from "@/components/ui/menu";
 import { AUTH_BASE_URL } from "@/lib/config/env.config";
 import { useOrganization } from "@/providers/OrganizationProvider";
 
@@ -33,47 +35,54 @@ const OrganizationSwitcher = () => {
   if (!hasMultipleOrgs) return null;
 
   return (
-    <Menu
-      trigger={
-        <Button variant="ghost" size="sm">
-          <Icon
-            src={currentOrganization.type === "personal" ? LuUser : LuBuilding2}
-          />
-          {currentOrganization.slug}
-          <Icon src={LuChevronDown} />
-        </Button>
-      }
+    <MenuRoot
       onSelect={(details) => {
         const orgId = details.value;
         if (orgId) setCurrentOrganization(orgId);
       }}
     >
-      <MenuItemGroup>
-        {organizations.map((org) => (
-          <MenuItem
-            key={org.id}
-            value={org.id}
-            disabled={org.id === currentOrganization.id}
-          >
-            <Icon src={org.type === "personal" ? LuUser : LuBuilding2} />
-            {org.slug}
-            {org.type === "personal" && " (Personal)"}
-          </MenuItem>
-        ))}
-      </MenuItemGroup>
+      <MenuTrigger asChild>
+        <Button variant="ghost" size="sm">
+          {currentOrganization.type === "personal" ? (
+            <LuUser />
+          ) : (
+            <LuBuilding2 />
+          )}
+          {currentOrganization.slug}
+          <LuChevronDown />
+        </Button>
+      </MenuTrigger>
 
-      <MenuSeparator />
+      <MenuPositioner>
+        <MenuContent>
+          <MenuItemGroup>
+            {organizations.map((org) => (
+              <MenuItem
+                key={org.id}
+                value={org.id}
+                disabled={org.id === currentOrganization.id}
+              >
+                {org.type === "personal" ? <LuUser /> : <LuBuilding2 />}
+                {org.slug}
+                {org.type === "personal" && " (Personal)"}
+              </MenuItem>
+            ))}
+          </MenuItemGroup>
 
-      {/* TODO: Implement in-app organization creation once Gatekeeper API supports it */}
-      <MenuItemGroup>
-        <MenuItem value="manage-orgs" asChild>
-          <a href={AUTH_BASE_URL}>
-            <Icon src={LuPlus} />
-            Manage Organizations
-          </a>
-        </MenuItem>
-      </MenuItemGroup>
-    </Menu>
+          <MenuSeparator />
+
+          {/* TODO: Implement in-app organization creation once Gatekeeper API supports it */}
+          <MenuItemGroup>
+            <MenuItem value="manage-orgs" asChild>
+              <a href={AUTH_BASE_URL}>
+                <LuPlus />
+                Manage Organizations
+              </a>
+            </MenuItem>
+          </MenuItemGroup>
+        </MenuContent>
+      </MenuPositioner>
+    </MenuRoot>
   );
 };
 
